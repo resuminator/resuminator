fb5fb71cea80cf66ac3985c2d0200dfe88294667
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-12 13:51:38 +0530

    Simulating Form Data

---
+++ src/components/Data/EducationInfo.js
@@ -0,0 +1,11 @@
+const educationInfo = [
+    {
+        id: 1,
+        institute: "Birla Institute of Technology, Mesra",
+        location: "Ranchi, Jharkhand",
+        stream: "Mathematics and Computing",
+        grade: 8.13
+    }
+]
+
+export default educationInfo
---
+++ src/components/Data/ProjectsInfo.js
@@ -0,0 +1,16 @@
+const projectsInfo = [
+    {
+        id: 1,
+        projectTitle: "Gromnom - Food Pooling App",
+        company: "Hack-a-BIT 2.0",
+        additionalInfo: "Won Special Mention Award",
+        start: "Oct 2019",
+        end: "",
+        description: [
+          "Built in 36 hours, from sratch to MVP.",
+        ],
+        tags: ['Flutter', 'Dart', 'Native-App Dev']
+      },
+]
+
+export default projectsInfo
