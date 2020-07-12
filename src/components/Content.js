8e2d851bfe78f1cdbab90eb2817d824876abe478
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-13 00:04:17 +0530

    Added Content and Title Component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Resume from "./Resume/Resume";
+import { Button, makeStyles, Tooltip } from "@material-ui/core";
+import NewWindow from "react-new-window";
 
-const styles = {
+const useStyles = makeStyles({
   root: {
     display: "flex",
     alignItems: "flex-start",
-    justifyContent: "flex-end"
+    justifyContent: "space-between",
   },
-};
+  btn: {
+    marginLeft: 40,
+  },
+});
+
+const printConfig = makeStyles({
+  root: {
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    //alignItems: "center",
+    zoom: 1.35,
+    width: 595,
+    height: 842,
+    backgroundColor: "#fffffe",
+    fontFamily: "Karla",
+    margin: 0,
+    right: 0,
+  },
+});
 
 function Content() {
+  const [print, showPrint] = useState(false);
+  const classes = useStyles();
+  const config = printConfig();
+
   return (
-    <div id="main" style={styles.root}>
+    <div id="main" className={classes.root}>
+      <Tooltip title="Click to see printable resume">
+        <Button className={classes.btn} onClick={() => showPrint(!print)}>
+          Print Resume
+        </Button>
+      </Tooltip>
+      {print ? (
+        <NewWindow title="Your Resume">
+          <Resume config={config.root} />
+        </NewWindow>
+      ) : null}
       <Resume />
     </div>
   );
--- src/components/Resume/Title/Title.js
+++ src/components/Resume/Title/Title.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center" 
+  },
+  name: {
+    color: "#121212",
+    fontSize: 35,
+    padding: 5,
+  },
+  jobtitle: {
+    color: "#44318D",
+    fontSize: 14,
+  },
+});
 
 function Title(props) {
+  const classes = useStyles();
   return (
-    <div>
-      <div style={{ color: "#121212", fontSize: 35, padding: 5 }}>
-        {props.name}
-      </div>
-      <div style={{ color: "#44318D", fontSize: 14 }}>
-        {props.jobTitle}
-      </div>
+    <div className={classes.root}>
+      <div className={classes.name}>{props.name}</div>
+      <div className={classes.jobtitle}>{props.jobTitle}</div>
     </div>
   );
 }
