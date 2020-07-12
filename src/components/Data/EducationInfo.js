16c22ad0bb5676c9333a384299cb19365ea38581
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-12 15:50:18 +0530

    Updated Education Info

--- src/components/Data/EducationInfo.js
+++ src/components/Data/EducationInfo.js
@@ -1,11 +1,15 @@
 const educationInfo = [
-    {
-        id: 1,
-        institute: "Birla Institute of Technology, Mesra",
-        location: "Ranchi, Jharkhand",
-        stream: "Mathematics and Computing",
-        grade: 8.13
-    }
-]
+  {
+    id: 1,
+    institute: "Birla Institute of Technology, Mesra",
+    location: "Ranchi, Jharkhand",
+    stream: "Mathematics and Computing",
+    grade: 8.13,
+    start: "2017",
+    end: "Present",
+    description:
+      "Ehsaas Dramatics Society, Literary Society, and Entrepreneurship Development Cell. Cadet in 3 JHAR CTC NCC",
+  },
+];
 
-export default educationInfo
+export default educationInfo;
