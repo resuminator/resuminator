a669c8a3f8e0764c838dcee7b004aec43763f75a
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-12 14:08:09 +0530

    Fixing Key prop error

--- src/components/Resume/Experience/Tags.js
+++ src/components/Resume/Experience/Tags.js
@@ -2,29 +2,33 @@ import React from "react";
 import { Chip, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
-    main: {
-        display: "flex",
-        justifyContent: "flex-start",
-        alignItems: "flex-start",
-    },
-    root: {
-        backgroundColor: "#44318D",
-        color: "#fffefa",
-        marginRight: 5,
-        marginTop: 5,
-        zoom: 0.75, 
-        opacity: 0.5
-    }
-})
+  main: {
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
+  root: {
+    backgroundColor: "#44318D",
+    color: "#fffefa",
+    marginRight: 5,
+    marginTop: 5,
+    zoom: 0.75,
+    opacity: 0.5,
+  },
+});
 
 function Tags(props) {
-    const classes = useStyles();
+  const classes = useStyles();
+  let key = 0;
   const tags = props.tags;
   return (
     <div className={classes.main}>
-      {tags.map((item) => (
-        <Chip className={classes.root} size="small" label={item} />
-      ))}
+      {tags.map((item) => {
+        ++key;
+        return (
+          <Chip key={key} className={classes.root} size="small" label={item} />
+        );
+      })}
     </div>
   );
 }
