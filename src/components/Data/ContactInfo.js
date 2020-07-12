14bbfc05c2e11660f0608159003b692d6041a5d9
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-12 13:41:39 +0530

    Simulating Form Data

---
+++ src/components/Data/ContactInfo.js
@@ -0,0 +1,20 @@
+const contactInfo = [
+    {
+        name: "github",
+        handle: "viveknigam3003"        
+    },
+    {
+        name: "twitter",
+        handle: "_viveknigam_"
+    },
+    {
+        name: "linkedin",
+        handle: "viveknigam3003"
+    },
+    {
+        name: "email",
+        handle: "viveknigam.nigam3@gmail.com"
+    }
+]
+
+export default contactInfo
---
+++ src/components/Data/ExperienceInfo.js
@@ -0,0 +1,43 @@
+const experienceInfo = [
+  {
+    id: 1,
+    jobTitle: "Student Developer",
+    company: "CERN-HSF",
+    additionalInfo: "GSoC 2020",
+    start: "May 2020",
+    end: "Present",
+    location: "Meyrin, Switzerland",
+    description: [
+      "Currently working as a Google Summer of Code Student Developer for Rucio - Exascale Data Management to develop a Native Desktop Application for Rucio.",
+    ],
+    tags: ['React', 'NodeJS', 'ElectronJS']
+  },
+  {
+    id: 2,
+    jobTitle: "Summer Research Fellow",
+    company: "IIT Bombay",
+    additionalInfo: "IAS-SRFP",
+    start: "May 2019",
+    end: "Jul 2019",
+    location: "Mumbai, Maharashtra",
+    description: [
+      "I worked as a SRF under Indian Academy of Sciences' (IAS) Summer Research Fellowship Programme-2019","As a research fellow, my job included reading research papers and text material related to Statistics and Game Theory, report my understanding to my guide through weekly presentations and discussions.","My 24-page report titled 'Strategic Aspects of Linear Regression' about perceiving Linear Regression as a non-cooperative game, was accepted by IAS.",
+    ],
+    tags: ["Game Theory", "Linear Regression"]
+  },
+  {
+    id: 3,
+    jobTitle: "Winter Intern",
+    company: "Linux World Informatics Pvt Ltd",
+    additionalInfo: "",
+    start: "Dec 2018",
+    end: "Jan 2019",
+    location: "Jaipur, Rajasthan",
+    description: [
+      "Worked under the direct guidance of Mr Vimal Daga. Learned and spent my time gaining hands-on experience of several trending technologies through trainings and mini-projects",
+    ],
+    tags: ["Big Data", "Hadoop", "Dockers"]
+  },
+];
+
+export default experienceInfo;
