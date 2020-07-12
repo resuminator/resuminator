a42609440d7828c518f109cad9875387b5afaf90
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-13 00:02:36 +0530

    Improved Line Component

--- src/components/Line.js
+++ src/components/Line.js
@@ -8,6 +8,10 @@ export default function ColoredLine(props) {
         backgroundColor: props.color,
         width: "100%",
         height: 1,
+        opacity: props.opacity || 1,
+        borderStyle: "inset",
+        borderWidth: 0,
+        overflow: "hidden"
       }}
     />
   );
