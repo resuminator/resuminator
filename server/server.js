4cb1c01305addc8e8ac481b874af5b0366056f87
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-19 21:37:46 +0530

    Added database connection

--- server/server.js
+++ server/server.js
@@ -10,6 +10,19 @@ const port = process.env.PORT || 4001;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("[INFO: DB] MongoDB database connection estabilished successfully");
+})
+
+const experienceRouter = require('./routes/experience');
+const userRouter = require('./routes/users');
+
+app.use('/experience', experienceRouter);
+app.use('/users', userRouter);
+
 app.listen(port, () => {
-    console.log(`[INFO] Resuminator Server running at ${port}`)
+    console.log(`[INFO: EX] Resuminator Server running at ${port}`)
 })
