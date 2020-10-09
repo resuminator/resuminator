3ad502cb421b4b7c0674c838ddee13ae4d4d1e01
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-10-09 12:46:39 +0530

    Displaying Work Link

--- src/components/Resume/Experience/JobDescription.js
+++ src/components/Resume/Experience/JobDescription.js
@@ -1,26 +1,48 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
+import { Link, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
-    root: {
-        textAlign: "justify",
-        paddingTop: 2,
-        lineHeight: 1.3
-    },
-    item:{
-        fontFamily: "Roboto",
-        fontSize: "0.9em",
-        opacity: 0.8,
-        marginBottom: 8
-    }
-})
+  root: {
+    textAlign: "justify",
+    paddingTop: 2,
+    lineHeight: 1.3,
+  },
+  item: {
+    fontFamily: "Roboto",
+    fontSize: "0.9em",
+    opacity: 0.8,
+    marginBottom: 8,
+  },
+  link: {
+    fontFamily: "Karla",
+    fontSize: "0.9em",
+    opacity: 0.8,
+    marginBottom: 8,
+    fontWeight: 600,
+  },
+});
 
 function JobDescription(props) {
-const classes = useStyles();
-let desc = props.desc;
+  const classes = useStyles();
+  let desc = props.desc;
   return (
     <div className={classes.root}>
-      {desc.map((item, i) => {i++; return <div className={classes.item} key={i}>• {item}</div>})}
+      {desc.map((item, i) => {
+        i++;
+        return (
+          <div className={classes.item} key={i}>
+            • {item}
+          </div>
+        );
+      })}
+      <Link
+        href={props.workLink}
+        className={classes.link}
+        color="#44318D"
+        target="_blank"
+      >
+        Work Link
+      </Link>
     </div>
   );
 }
