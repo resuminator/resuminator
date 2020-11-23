be786cbdadbe51dac3550e465017e5e6487f0f2a
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-11-23 20:26:20 +0530

    Updated User-reducer

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,10 +1,8 @@
 import { USERINFO } from "../actionTypes";
 
 const initialState = {
-  userInfo: {
     name: "",
     jobtitle: "",
-  },
 };
 
 const userReducer = (state = initialState, action) => {
@@ -13,7 +11,7 @@ const userReducer = (state = initialState, action) => {
       const { name, jobtitle } = action.payload;
       return {
         ...state,
-        name,
+        name, 
         jobtitle,
       };
     }
