4b884fd7aeae8676f0b7f8eabccca835c10e23d6
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-11-24 18:13:40 +0530

    Cleaned Exp Input component

--- src/components/UserInput/components/Experience/ExperienceInfo.js
+++ src/components/UserInput/components/Experience/ExperienceInfo.js
@@ -11,6 +11,7 @@ import React, { useState } from "react";
 import { FiPlus } from "react-icons/fi";
 import { useDispatch, useSelector } from "react-redux";
 import { EXPERIENCE_INFO } from "../../../../redux/actionTypes";
+import { parseDate } from "../../../utils/Helpers";
 
 const useStyles = makeStyles((theme) => ({
   TextField: {
@@ -55,33 +56,6 @@ function ExperienceInfo() {
   const [experience, setExperience] = useState({description: ``});
   const [currentIndex, setCurrentIndex] = useState(0);
   const experiences = useSelector((state) => state.experienceInfo);
-
-  const getMonth = (month) => {
-    switch (month){
-      case "01": return "Jan"
-      case "02": return "Feb"
-      case "03": return "Mar"
-      case "04": return "Apr"
-      case "05": return "May"
-      case "06": return "Jun"
-      case "07": return "Jul"
-      case "08": return "Aug"
-      case "09": return "Sep"
-      case "10": return "Oct"
-      case "11": return "Nov"
-      case "12": return "Dec"
-      default: return ""
-    }
-  }
-
-  const parseDate = (type, value) => {
-    if(type === "date"){
-      const date = value.split("-", 2);
-      return getMonth(date[1]).concat(" ", date[0].substring(2, 5))
-    }
-    
-    return value;
-  }
 
   const handleChange = (e, index) => {
     e.preventDefault();
