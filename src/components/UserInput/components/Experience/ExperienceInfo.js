de7230fef7604aae81efebf40f37b971932c79c8
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-11-24 14:24:19 +0530

    Parsing Dates

--- src/components/UserInput/components/Experience/ExperienceInfo.js
+++ src/components/UserInput/components/Experience/ExperienceInfo.js
@@ -56,10 +56,37 @@ function ExperienceInfo() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const experiences = useSelector((state) => state.experienceInfo);
 
+  const getMonth = (month) => {
+    switch (month){
+      case "01": return "Jan"
+      case "02": return "Feb"
+      case "03": return "Mar"
+      case "04": return "Apr"
+      case "05": return "May"
+      case "06": return "Jun"
+      case "07": return "Jul"
+      case "08": return "Aug"
+      case "09": return "Sep"
+      case "10": return "Oct"
+      case "11": return "Nov"
+      case "12": return "Dec"
+      default: return ""
+    }
+  }
+
+  const parseDate = (type, value) => {
+    if(type === "date"){
+      const date = value.split("-", 2);
+      return getMonth(date[1]).concat(" ", date[0].substring(2, 5))
+    }
+    
+    return value;
+  }
+
   const handleChange = (e, index) => {
     e.preventDefault();
     const field = e.target.name;
-    const value = e.target.value;
+    const value = parseDate(e.target.type, e.target.value);
 
     setExperience({ ...experience, [field]: value });
     setCurrentIndex(index);
@@ -72,12 +99,6 @@ function ExperienceInfo() {
       index: currentIndex,
     });
   }, [dispatch, experience, currentIndex]);
-
-  
-  // React.useEffect(()=> {
-  //   const index = experiences.length - 1;
-  //   dispatch({ type: EXPERIENCE_INFO.ADD, index: index });
-  // },[dispatch, experiences]);
 
   return (
     <Box display="flex" flexDirection="column" mt={1} p={2}>
