c7c43189a0fe8de4324d8ead3e3c0cdc951e13fc
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-12-08 13:29:37 +0530

    Fixed Confirm Button Styling

--- src/components/common/ConfirmButton.js
+++ src/components/common/ConfirmButton.js
@@ -1,20 +1,29 @@
-import { IconButton } from "@material-ui/core";
+import { IconButton, makeStyles } from "@material-ui/core";
 import { AnimateSharedLayout, motion } from "framer-motion";
 import React from "react";
 import { FiArrowUp, FiCheck } from "react-icons/fi";
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginBottom: 0,
+    margin: theme.spacing(1),
+    padding: theme.spacing(1)
+  }
+}))
+
 function ConfirmButton({ onClick, changed }) {
+  const classes = useStyles();
   return (
     <AnimateSharedLayout>
       {changed ? (
         <motion.div layoutId="confirm-button">
-          <IconButton color="primary" onClick={onClick}>
+          <IconButton size="medium" className={classes.root} color="primary" onClick={onClick}>
             <FiCheck />
           </IconButton>
         </motion.div>
       ) : (
         <motion.div layoutId="confirm-button">
-          <IconButton onClick={onClick}>
+          <IconButton size="medium" className={classes.root} onClick={onClick}>
             <FiArrowUp color="primary" />
           </IconButton>
         </motion.div>
