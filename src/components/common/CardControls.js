15d6323779393938ccae08d281cc54b57390c5e8
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-12-08 13:33:57 +0530

    Added License Header

--- src/components/common/CardControls.js
+++ src/components/common/CardControls.js
@@ -1,4 +1,14 @@
 import { Box } from "@material-ui/core";
+/*
+ * Copyright Vivek Nigam, 2020
+ * Licensed under the GNU General Public License, Version 3.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Authors:
+ * - Vivek Nigam, <viveknigam.nigam3@gmail.com>, 2020
+ */
+
 import React from "react";
 
 const CardControls = ({ children }) => {
--- src/redux/app.actions.js
+++ src/redux/app.actions.js
@@ -1,3 +1,13 @@
+/*
+ * Copyright Vivek Nigam, 2020
+ * Licensed under the GNU General Public License, Version 3.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Authors:
+ * - Vivek Nigam, <viveknigam.nigam3@gmail.com>, 2020
+ */
+
 import { fetchExperience } from "../components/Experience/experience.actions";
 import { fetchUser } from "../components/Title/title.actions";
 
