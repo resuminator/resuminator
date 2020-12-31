fd5f4da1e3262c658d271809a5b51bd5228f0781
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-12-31 12:03:05 +0530

    Fixed Redux Dev Tools in Prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,7 @@
  */
 
 import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
