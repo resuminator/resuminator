aeeb99309bc9af999a749342912b76bae862d886
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-01-07 21:40:13 +0530

    Updated Welcome Dialog

--- src/App/WelcomeDialog.js
+++ src/App/WelcomeDialog.js
@@ -1,27 +1,19 @@
-import React from 'react'
-import { AlertDialog } from '../components/common/AlertDialog';
+import React from "react";
+import { AlertDialog } from "../components/common/AlertDialog";
+import { welcomeMessage } from "./Messages";
 
-const WelcomeDialog = ({open,
-    onClick,
-    onClose}) => {
-    const message = {
-        title: "Welcome to Resuminator! 👋🏻",
-        message: `Thanks for joining Resuminator Early Access Programme! 
-        Currently, Resuminator is in Beta stage - 
-        this means that you may play around the app and we would love to know your feedback to improve!`,
-        actionText: "Got it! 👍🏻",
-      };
+const WelcomeDialog = ({ open, onClick, onClose }) => {
+  const message = welcomeMessage;
+  return (
+    <AlertDialog
+      title={message.title}
+      message={message.message}
+      open={open}
+      onClick={onClick}
+      onClose={onClose}
+      buttonText={message.actionText}
+    />
+  );
+};
 
-    return (
-        <AlertDialog
-        title={message.title}
-        message={message.message}
-        open={open}
-        onClick={onClick}
-        onClose={onClose}
-        buttonText={message.actionText}
-      />
-    )
-}
-
-export default WelcomeDialog
+export default WelcomeDialog;
