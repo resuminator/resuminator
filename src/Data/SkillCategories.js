7accdf692b3a96e06498378bf8d9bc8aa0009171
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-01-29 16:22:15 +0530

    Added Skill Categories

---
+++ src/Data/SkillCategories.js
@@ -0,0 +1,16 @@
+export const AllSkills = [
+  "Programming Languages",
+  "Machine Learning & AI",
+  "Data Science & Analytics",
+  "Front-end Development",
+  "App Development",
+  "Back-end Development",
+  "DevOps",
+  "UX Design",
+  "Designing",
+  "Tools",
+  "Soft Skills",
+  "Mathematical Skills",
+  "Software Development",
+  "Cloud Development"
+];
