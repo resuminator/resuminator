941a5b6d0fb921ae9128b0b1b4c0a27a3e2c7326
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-06 22:40:17 +0530

    Added Login Page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,38 @@
 import { Box, Text } from "@chakra-ui/layout";
 import { NextPage } from "next";
 import React from "react";
+import AuthProviderCard from "../components/common/AuthProviderCard";
 import Layout from "../components/layouts";
 
 const Login: NextPage = () => {
   return (
     <Layout>
-      <Box flex="1 0" flexBasis="50%" bg="red">
-        <Text fontSize="4xl">Col 1</Text>
+      <Box
+        display={{ base: "none", lg: "inherit" }}
+        flexDir="column"
+        flex="1 0"
+        flexBasis="60%"
+        p={{ base: "2rem", md: "4rem", lg: "4rem 2rem" }}
+      >
+        <Text fontSize="2xl" fontWeight="semibold" color="blue.800">
+          Resuminator
+        </Text>
+        <Text fontSize="sm" color="blue.500">
+          Build beautiful single-page resumes within minutes
+        </Text>
       </Box>
-      <Box flexBasis="50%" bg="blue">
-        <Text fontSize="4xl">Col 2</Text>
+      <Box
+        display="flex"
+        flexDir="column"
+        p={{ base: "2rem", md: "4rem", lg: "4rem 2rem" }}
+        flex="1 0"
+        flexBasis="40%"
+        bg="whiteAlpha.100"
+      >
+        <AuthProviderCard client="Google" />
+        <AuthProviderCard client="Twitter" />
+        <AuthProviderCard client="GitHub" />
+        <AuthProviderCard client="Email" />
       </Box>
     </Layout>
   );
