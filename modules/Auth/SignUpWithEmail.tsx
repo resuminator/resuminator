1c6ee3c261a136b71da475afa848365cff586176
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-07 19:45:55 +0530

    Handling state in email signup

--- modules/Auth/SignUpWithEmail.tsx
+++ modules/Auth/SignUpWithEmail.tsx
@@ -1,13 +1,40 @@
 import { Button } from "@chakra-ui/button";
+import { SyntheticEvent, useState } from "react";
 import { FaChevronLeft } from "react-icons/fa";
 import InputField from "../../components/common/InputField";
 import MotionBox from "../../components/layouts/MotionBox";
+import { usePasswordValidation } from "../../hooks/usePasswordValidation";
+import PasswordHints from "./PasswordHints";
 
+export interface FormValues {
+  fullName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 interface Props {
   resetClient: () => void;
+  formValues: FormValues;
+  formHandler: (e: SyntheticEvent) => void;
+  submitHandler?: () => void;
 }
 
-const SignUpWithEmail: React.FC<Props> = ({ resetClient }) => {
+const SignUpWithEmail: React.FC<Props> = ({
+  resetClient,
+  formValues,
+  formHandler,
+  submitHandler,
+}) => {
+  const [
+    validLength,
+    hasNumber,
+    upperCase,
+    lowerCase,
+    match,
+  ] = usePasswordValidation(formValues.password, formValues.confirmPassword);
+  const [showHints, setShowHints] = useState(false);
+  const [showConfirmHint, setShowConfirmHint] = useState(false);
+
   return (
     <MotionBox
       initial={{ opacity: 0 }}
@@ -15,16 +42,55 @@ const SignUpWithEmail: React.FC<Props> = ({ resetClient }) => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <InputField label="Full Name" type="text"/>
-      <InputField label="Email" type="email" />
-      <InputField label="Password" type="password"/>
-      <InputField label="Confirm Password" type="password"/>
+      <InputField
+        label="Full Name"
+        name="fullName"
+        value={formValues.fullName}
+        type="text"
+        onChange={formHandler}
+      />
+      <InputField
+        label="Email"
+        name="email"
+        value={formValues.email}
+        type="email"
+        onChange={formHandler}
+      />
+      <InputField
+        label="Password"
+        name="password"
+        value={formValues.password}
+        type="password"
+        onChange={formHandler}
+        onFocus={() => setShowHints(true)}
+        onBlur={() => setShowHints(false)}
+      />
+      {showHints ? (
+        <PasswordHints
+          validators={{ validLength, hasNumber, upperCase, lowerCase }}
+        />
+      ) : null}
+      <InputField
+        label="Confirm Password"
+        name="confirmPassword"
+        value={formValues.confirmPassword}
+        type="password"
+        onChange={formHandler}
+        onFocus={() => setShowConfirmHint(true)}
+        onBlur={() => setShowConfirmHint(false)}
+      />
+      {showConfirmHint && !match ? (
+        <PasswordHints
+          hints={[{ message: "Passwords don't match", validator: match }]}
+        />
+      ) : null}
       <Button
         isFullWidth
         variant="solid"
         colorScheme="blue"
         textAlign="center"
         mb="4"
+        onClick={submitHandler}
       >
         Create new account
       </Button>
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -7,10 +7,26 @@ import LogoWithText from "../components/layouts/LogoWithText";
 import AuthProvidersList from "../modules/Auth/AuthProvidersList";
 import PageToggle from "../modules/Auth/PageToggle";
 import PrivacyNotice from "../modules/Auth/PrivacyNotice";
-import SignUpWithEmail from "../modules/Auth/SignUpWithEmail";
+import SignUpWithEmail, { FormValues } from "../modules/Auth/SignUpWithEmail";
 
 const Signup = () => {
   const [client, setClient] = useState(null);
+  const [formValues, setFormValues] = useState<FormValues>({
+    fullName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    const [key, value] = [e.target.name, e.target.value];
+    setFormValues({ ...formValues, [key]: value });
+  };
+
+  const handleSubmit = () => {
+    console.log(formValues);
+  };
 
   return (
     <Layout>
@@ -28,7 +44,12 @@ const Signup = () => {
         />
         <AnimatePresence>
           {client === "Email" ? (
-            <SignUpWithEmail resetClient={() => setClient(null)} />
+            <SignUpWithEmail
+              resetClient={() => setClient(null)}
+              formValues={formValues}
+              formHandler={handleForm}
+              submitHandler={handleSubmit}
+            />
           ) : (
             <AuthProvidersList setClient={setClient} />
           )}
