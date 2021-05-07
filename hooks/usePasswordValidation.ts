92ec23de29eae5284eec4f0762e27cf1c909c86b
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-07 18:28:32 +0530

    Added password validation custom hook

---
+++ hooks/usePasswordValidation.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export const usePasswordValidation = (
+  password: string,
+  confirmPassword: string,
+  reqLength = 8
+) => {
+  const [validLength, setValidLength] = useState(null);
+  const [hasNumber, setHasNumber] = useState(null);
+  const [upperCase, setUpperCase] = useState(null);
+  const [lowerCase, setLowerCase] = useState(null);
+  const [match, setMatch] = useState(null);
+
+  useEffect(() => {
+    setValidLength(password.length >= reqLength ? true : false);
+    setUpperCase(password.toLowerCase() !== password);
+    setLowerCase(password.toUpperCase() !== password);
+    setHasNumber(/\d/.test(password));
+    setMatch(password && password === confirmPassword);
+  }, [password, confirmPassword, reqLength]);
+
+  return [validLength, hasNumber, upperCase, lowerCase, match];
+};
