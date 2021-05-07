c01814e2fe3ade6c649a175963304e83988ce939
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-07 12:15:09 +0530

    LogInWithEmail Layout improved

--- modules/Auth/LoginWithEmail.tsx
+++ modules/Auth/LoginWithEmail.tsx
@@ -1,10 +1,19 @@
+import { Button } from "@chakra-ui/button";
 import { Fragment } from "react";
+import { FaChevronLeft } from "react-icons/fa";
 import InputField from "../../components/common/InputField";
 
-const LogInWithEmail = () => (
+interface Props {
+  resetClient: () => void;
+}
+
+const LogInWithEmail: React.FC<Props> = ({ resetClient }) => (
   <Fragment>
     <InputField label="Email" />
     <InputField label="Password" />
+    <Button variant="link" onClick={resetClient} my="2">
+      <FaChevronLeft style={{paddingRight: "8px"}}/> Back to all options
+    </Button>
   </Fragment>
 );
 
