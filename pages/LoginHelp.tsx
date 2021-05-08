177853c3c607b5d57459e4df594c05a929252b7a
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-08 12:26:58 +0530

    Handling input and mock submit

--- pages/LoginHelp.tsx
+++ pages/LoginHelp.tsx
@@ -1,13 +1,39 @@
 import { Button } from "@chakra-ui/button";
 import { Box } from "@chakra-ui/layout";
+import { useToast } from "@chakra-ui/toast";
 import { NextPage } from "next";
 import Link from "next/link";
+import { useState } from "react";
+import { AiOutlineMail } from "react-icons/ai";
 import { FaChevronLeft } from "react-icons/fa";
 import BoxHeader from "../components/common/BoxHeader";
 import InputField from "../components/common/InputField";
 import ScreenCenter from "../components/layouts/ScreenCenter";
+import { useEmailValidation } from "../hooks/useEmailValidation";
 
 const LoginHelp: NextPage = () => {
+  const [email, setEmail] = useState("");
+  const [validEmail, errorMessage] = useEmailValidation(email);
+  const [showError, setShowError] = useState(false);
+  const toast = useToast();
+
+  const handleInputField = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    //handle forgot password logic here
+    console.log(email);
+    toast({
+      title: "Email Sent",
+      description:
+        "Check your inbox for further instructions. You may return to log in page now.",
+      status: "info",
+      duration: 5000,
+      isClosable: true,
+    });
+  };
+
   return (
     <ScreenCenter>
       <Box
@@ -23,13 +49,29 @@ const LoginHelp: NextPage = () => {
           title="Forgot Password?"
           subtitle="Enter your email for instructions on resetting your password"
         />
-        <InputField label="Email" />
+        <InputField
+          label="Email"
+          type="email"
+          onChange={handleInputField}
+          isValid={validEmail}
+          error={{ message: errorMessage, show: showError && !validEmail }}
+          onFocus={() => setShowError(true)}
+        />
+        <Button
+          fontWeight="medium"
+          variant="solid"
+          colorScheme="purple"
+          mt="2"
+          onClick={handleSubmit}
+        >
+          <AiOutlineMail style={{ marginRight: "8px" }} /> Send Instructions
+        </Button>
         <Link href="/login">
           <Button
             fontWeight="medium"
-            variant="solid"
-            colorScheme="purple"
-            mt="2"
+            colorScheme="blackAlpha"
+            variant="ghost"
+            my="4"
           >
             <FaChevronLeft style={{ paddingRight: "8px" }} /> Back to log in
             page
