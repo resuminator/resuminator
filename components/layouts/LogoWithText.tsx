0d597ab958e427033d5b5b4382a3634f0b6a19aa
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-09 14:25:07 +0530

    Added Logo Props

--- components/layouts/LogoWithText.tsx
+++ components/layouts/LogoWithText.tsx
@@ -1,18 +1,30 @@
-import { Box, BoxProps, Text } from "@chakra-ui/layout";
+import { Box, BoxProps, Text, TextProps } from "@chakra-ui/layout";
 
 interface Props {
-  hasTagline?: boolean
+  hasTagline?: boolean;
+  LogoProps?: TextProps;
 }
 
-const LogoWithText: React.FC<Props & BoxProps> = ({ hasTagline = false, ...props }) => {
+const LogoWithText: React.FC<Props & BoxProps> = ({
+  hasTagline = false,
+  LogoProps,
+  ...props
+}) => {
   return (
     <Box display="flex" flexDir="column" mb="8" {...props}>
-      <Text fontSize="2xl" fontWeight="semibold" color="blue.800">
+      <Text
+        fontSize="2xl"
+        fontWeight="semibold"
+        color="blue.800"
+        {...LogoProps}
+      >
         Resuminator
       </Text>
-     {hasTagline && <Text fontSize="sm" color="blue.500">
-        Build beautiful single-page resumes within minutes
-      </Text>}
+      {hasTagline && (
+        <Text fontSize="sm" color="blue.500">
+          Build beautiful single-page resumes within minutes
+        </Text>
+      )}
     </Box>
   );
 };
