a9a0c9be10fdfcf6f566d45c67d52d762c78bbea
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-10 21:01:28 +0530

    Updated Create Page Composition

--- pages/create.tsx
+++ pages/create.tsx
@@ -2,10 +2,9 @@ import { Box } from "@chakra-ui/layout";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
-import InputField from "../components/common/InputField";
 import Layout from "../components/layouts";
 import NavTabs from "../components/layouts/NavTabs";
-import Section from "../components/layouts/Section";
+import { Contact, NameAndJobTitle } from "../modules/UserInput";
 
 const Create: NextPage = () => {
   const router = useRouter();
@@ -21,21 +20,12 @@ const Create: NextPage = () => {
         py="5"
       >
         <Box aria-label="Resume Inputs" flexBasis="50%">
-          {[1, 2, 3, 4].map((item) => (
-            <Section
-              key={item}
-              header={{
-                title: "Let's go over some basic info",
-                subtitle: "Provide your name, email, and where to contact you",
-                size: { title: "lg", subtitle: "sm" },
-              }}
-            >
-              <InputField labelProps={{ fontSize: "sm" }} label="Full Name" />
-              <InputField labelProps={{ fontSize: "sm" }} label="Job Title" />
-            </Section>
-          ))}
+          <NameAndJobTitle />
+          <Contact />
         </Box>
-        <Box aria-label="Resume Preview" flexBasis="50%">Resume</Box>
+        <Box aria-label="Resume Preview" flexBasis="50%">
+          Resume
+        </Box>
       </Box>
     </Layout>
   );
