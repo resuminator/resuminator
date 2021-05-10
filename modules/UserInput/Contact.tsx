93861ca22478f94fde0cddf4288ee3fbe1a1ffef
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-10 22:35:58 +0530

    Added Contact Section

--- modules/UserInput/Contact.tsx
+++ modules/UserInput/Contact.tsx
@@ -3,21 +3,95 @@ import { Input } from "@chakra-ui/input";
 import { HStack, Text } from "@chakra-ui/layout";
 import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
 import { Tooltip } from "@chakra-ui/tooltip";
-import React from "react";
+import React, { ReactElement, useState } from "react";
+import { CgOptions } from "react-icons/cg";
 import {
   FaChevronDown,
   FaGithub,
   FaLink,
   FaLinkedin,
   FaPhoneAlt,
-  FaPlus,
   FaTwitter,
-  FaUserAlt,
+  FaUserAlt
 } from "react-icons/fa";
-import { FiMail } from "react-icons/fi";
+import { FiEye, FiMail } from "react-icons/fi";
+import { IoMdRemoveCircle } from "react-icons/io";
 import Section from "../../components/layouts/Section";
 
+type Services =
+  | "LinkedIn"
+  | "Twitter"
+  | "GitHub"
+  | "Email"
+  | "Phone"
+  | "Portfolio"
+  | "Custom"
+  | string;
+interface SocialHandleObject {
+  label: Services | string;
+  icon?: ReactElement;
+}
+
+const SocialHandles: Array<SocialHandleObject> = [
+  { label: "LinkedIn" },
+  { label: "Twitter" },
+  { label: "GitHub" },
+  { label: "Email" },
+  { label: "Phone" },
+  { label: "Portfolio" },
+  { label: "Custom" },
+];
+
+interface SelectedHandleObject extends SocialHandleObject {
+  isHidden: boolean;
+}
+
+const getIconForService = (label: Services) => {
+  switch (label) {
+    case "LinkedIn":
+      return <FaLinkedin />;
+    case "Twitter":
+      return <FaTwitter />;
+    case "GitHub":
+      return <FaGithub />;
+    case "Email":
+      return <FiMail />;
+    case "Phone":
+      return <FaPhoneAlt />;
+    case "Portfolio":
+      return <FaUserAlt />;
+    default:
+      return <FaLink />;
+  }
+};
+
+const getColorSchemeForService = (label: Services) => {
+  switch (label) {
+    case "LinkedIn":
+      return "linkedin";
+    case "Twitter":
+      return "twitter";
+    case "GitHub":
+      return "gray";
+    case "Email":
+      return "teal";
+    case "Phone":
+      return "green";
+    case "Portfolio":
+      return "purple";
+    default:
+      return "blue";
+  }
+};
+
 const Contact = () => {
+  const [data, setData] = useState([{ label: "LinkedIn" }]);
+  const [current, setCurrent] = useState<SelectedHandleObject>({
+    label: "LinkedIn",
+    icon: getIconForService("LinkedIn"),
+    isHidden: false,
+  });
+
   return (
     <Section
       header={{
@@ -25,29 +99,51 @@ const Contact = () => {
         subtitle: "Select the service and enter your social handle",
       }}
     >
-      <HStack>
-        <Menu>
-          <MenuButton as={Button} rightIcon={<FaChevronDown />}>
-            <HStack>
-              <FaLinkedin />
-              <Text>LinkedIn</Text>
-            </HStack>
-          </MenuButton>
-          <MenuList>
-            <MenuItem icon={<FaLinkedin />}>LinkedIn</MenuItem>
-            <MenuItem icon={<FaTwitter />}>Twitter</MenuItem>
-            <MenuItem icon={<FaGithub />}>GitHub</MenuItem>
-            <MenuItem icon={<FiMail />}>Email</MenuItem>
-            <MenuItem icon={<FaPhoneAlt />}>Phone</MenuItem>
-            <MenuItem icon={<FaUserAlt />}>Portfolio</MenuItem>
-            <MenuItem icon={<FaLink />}>Custom</MenuItem>
-          </MenuList>
-        </Menu>
-        <Input variant="outline" w="16rem" />
-        <Tooltip label="Add New Handle">
-          <IconButton aria-label="Add-New-Contact-Handle" icon={<FaPlus />} />
-        </Tooltip>
-      </HStack>
+      {data.map((item) => (
+        <HStack key={item.label}>
+          <Menu>
+            <MenuButton
+              as={Button}
+              rightIcon={<FaChevronDown />}
+              colorScheme={getColorSchemeForService(current.label)}
+            >
+              <HStack>
+                {getIconForService(current.label)}
+                <Text>{current.label}</Text>
+              </HStack>
+            </MenuButton>
+            <MenuList>
+              {SocialHandles.map((item) => (
+                <MenuItem
+                  key={item.label}
+                  icon={getIconForService(item.label)}
+                  onClick={() => setCurrent({ ...current, label: item.label })}
+                >
+                  {item.label}
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Menu>
+          <Input variant="outline" w="16rem" />
+          <Menu>
+            <Tooltip label="Options">
+              <MenuButton
+                as={IconButton}
+                aria-label="Show-Current-Handle-Options"
+                icon={<CgOptions />}
+              />
+            </Tooltip>
+            <MenuList>
+              <MenuItem icon={<FiEye />}>
+                {current.isHidden ? "Show on resume" : "Hide from resume"}
+              </MenuItem>
+              <MenuItem color="red.500" icon={<IoMdRemoveCircle />}>
+                Remove
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </HStack>
+      ))}
     </Section>
   );
 };
