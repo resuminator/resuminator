2470d0032a1ad15920038f4aa89860156446c0eb
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-12 22:27:26 +0530

    State changes working

--- modules/UserInput/Experience.tsx
+++ modules/UserInput/Experience.tsx
@@ -1,10 +1,11 @@
+import { Content } from "@tiptap/core";
 import React, { useEffect, useState } from "react";
 import { FiPlus } from "react-icons/fi";
 import EditorWithLabel from "../../components/common/EditorWithLabel";
 import InputWithLabel from "../../components/common/InputWithLabel";
 import TooltipIconButton from "../../components/common/TooltipIconButton";
 import Section from "../../components/layouts/Section";
-import { useTiptap } from "../../plugins/Tiptap";
+import { getUniqueID } from "../../utils";
 import ExpandableCard from "./ExpandableCard";
 import SectionControls, { SectionProperties } from "./SectionControls";
 
@@ -13,7 +14,7 @@ interface ExperienceDataObject {
   jobTitle?: string;
   company?: string;
   location?: string;
-  description?: string;
+  description?: Content;
   link?: string;
   tags?: Array<string>;
   start?: Date;
@@ -21,67 +22,57 @@ interface ExperienceDataObject {
   isHidden?: boolean;
 }
 
-interface DataObject extends ExperienceDataObject {
-  isExpanded?: boolean;
-}
-
-type DataState = Array<DataObject>;
-
-const DummyData = {
-  _id: "kasjdfw098hjkaf893nr0",
-  jobTitle: "Student Developer @ CERN-HSF",
-  company: "Google Summer of Code '20",
-  location: "Remote",
-  description: "<p>Testing Testing</p>",
-  link: "",
-  tags: ["GSoC", "React"],
-  start: new Date(),
-  end: new Date(),
-  isHidden: false,
-  isExpanded: true,
-};
+type DataState = Array<ExperienceDataObject>;
 
 const Experience = () => {
   const [properties, setProperties] = useState<SectionProperties>({
     isHidden: false,
   });
   const [data, setData] = useState<DataState>([]);
-  const { editor, output } = useTiptap("", {
-    outputFormat: "HTML",
-    placeholder: "Describe your role and achievements...",
-  });
+  const [currentObject, setCurrentObject] = useState<ExperienceDataObject>();
 
-  const handleAdd = () => {
-    setData([...data, DummyData]);
-    editor.commands.setContent(DummyData.description);
+  const DummyData: ExperienceDataObject = {
+    _id: getUniqueID(),
+    jobTitle: "Student Developer @ CERN-HSF",
+    company: "Google Summer of Code '20",
+    location: "Remote",
+    description: "<p>Testing Testing</p>",
+    link: "",
+    tags: ["GSoC", "React"],
+    start: new Date(),
+    end: new Date(),
+    isHidden: false,
+  };
+
+  //mocking data from DB.
+  const getNewObject = async () => {
+    return DummyData;
+  };
+
+  const handleAdd = async () => {
+    await getNewObject().then((res) => setData((data) => [...data, res]));
+  };
+
+  const toggleVisibilityOnResume = () => {
+    setCurrentObject((nextCurrent) => ({
+      ...nextCurrent,
+      isHidden: !currentObject.isHidden,
+    }));
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const [key, value] = [e.target.name, e.target.value];
+    setCurrentObject((nextCurrent) => ({ ...nextCurrent, [key]: value }));
   };
 
   useEffect(() => {
-    console.log(output);
-  }, [output]);
-
-  const toggleExpand = (id: string) => {
-    const current: DataObject = data.filter((item) => item._id === id)[0];
-    const nextCurrent: DataObject = {
-      ...current,
-      isExpanded: !current.isExpanded,
-    };
-    setData(data.map((item) => (item._id === id ? nextCurrent : item)));
-  };
-
-  const toggleVisibilityOnResume = (id: string) => {
-    const current: DataObject = data.filter((item) => item._id === id)[0];
-    const nextCurrent: DataObject = { ...current, isHidden: !current.isHidden };
-    setData(data.map((item) => (item._id === id ? nextCurrent : item)));
-  };
-
-  const handleChange = (e, id: string) => {
-    e.preventDefault();
-    const [key, value] = [e.target.name, e.target.value];
-    const current: DataObject = data.filter((item) => item._id === id)[0];
-    const nextCurrent: DataObject = { ...current, [key]: value };
-    setData(data.map((item) => (item._id === id ? nextCurrent : item)));
-  };
+    setData((nextData) =>
+      nextData.map((item) =>
+        item._id === currentObject._id ? currentObject : item
+      )
+    );
+  }, [currentObject]);
 
   return (
     <Section
@@ -104,49 +95,50 @@ const Experience = () => {
           key={item._id}
           title={item.company}
           subtitle={item.jobTitle}
-          expandHandler={{
-            value: item.isExpanded,
-            setValue: () => toggleExpand(item._id),
-          }}
           visibilityHandler={{
             value: item.isHidden,
-            setValue: () => toggleVisibilityOnResume(item._id),
+            setValue: () => toggleVisibilityOnResume(),
           }}
+          chainOnClick={() => setCurrentObject(item)}
         >
           <InputWithLabel
             label="Job Title"
             name="jobTitle"
             placeholder="Rocket Scientist"
             value={item.jobTitle}
-            onChange={(e) => handleChange(e, item._id)}
+            onChange={handleChange}
           />
           <InputWithLabel
             label="Organization"
-            name="jobTitle"
+            name="company"
             placeholder="Tesla"
             value={item.company}
-            onChange={(e) => handleChange(e, item._id)}
+            onChange={handleChange}
           />
           <InputWithLabel
             label="Location"
             name="location"
             placeholder="Start typing to search location"
             value={item.location}
-            onChange={(e) => handleChange(e, item._id)}
+            onChange={handleChange}
           />
-          <EditorWithLabel label="Description" editor={editor} />
+          <EditorWithLabel
+            onChange={setCurrentObject}
+            content={item.description}
+            label="Description"
+          />
           <InputWithLabel
             label="Tags"
             name="tags"
             placeholder="Separate keywords by commas"
             value={item.tags}
-            onChange={(e) => handleChange(e, item._id)}
+            onChange={handleChange}
           />
           <InputWithLabel
             label="Link"
             name="link"
             value={item.link}
-            onChange={(e) => handleChange(e, item._id)}
+            onChange={handleChange}
           />
         </ExpandableCard>
       ))}
