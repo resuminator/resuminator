10339e68fdbb40923f9a3c44bb786d1fccc16116
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-12 13:37:28 +0530

    Added Toggles for Editor

--- plugins/Tiptap/MenuBar.tsx
+++ plugins/Tiptap/MenuBar.tsx
@@ -1,9 +1,16 @@
-import { ButtonGroup, IconButton } from "@chakra-ui/button";
+import { ButtonGroup } from "@chakra-ui/button";
 import { HStack } from "@chakra-ui/layout";
 import { Editor } from "@tiptap/react";
 import React from "react";
-import { FaBold, FaItalic } from "react-icons/fa";
-
+import {
+  FaAlignJustify,
+  FaAlignLeft,
+  FaBold,
+  FaItalic,
+  FaListOl,
+  FaListUl
+} from "react-icons/fa";
+import TooltipIconButton from "../../components/common/TooltipIconButton";
 interface Props {
   editor: Editor;
 }
@@ -14,9 +21,10 @@ const MenuBar: React.FC<Props> = ({ editor }) => {
   }
 
   return (
-    <HStack spacing="1">
+    <HStack spacing="2">
       <ButtonGroup isAttached>
-        <IconButton
+        <TooltipIconButton
+          label="Bold"
           variant={editor.isActive("bold") ? "solid" : "outline"}
           size="xs"
           aria-label="Bold-Text"
@@ -24,13 +32,58 @@ const MenuBar: React.FC<Props> = ({ editor }) => {
           onClick={() => editor.chain().focus().toggleBold().run()}
           colorScheme={editor.isActive("bold") ? "blue" : "gray"}
         />
-        <IconButton
+        <TooltipIconButton
+          label="Italics"
           variant={editor.isActive("italic") ? "solid" : "outline"}
           size="xs"
-          aria-label="Bold-Text"
+          aria-label="Italics-Text"
           icon={<FaItalic />}
           onClick={() => editor.chain().focus().toggleItalic().run()}
           colorScheme={editor.isActive("italic") ? "blue" : "gray"}
+        />
+      </ButtonGroup>
+      <ButtonGroup isAttached>
+        <TooltipIconButton
+          label="Bullet List"
+          variant={editor.isActive("bulletList") ? "solid" : "outline"}
+          size="xs"
+          aria-label="Bullet-List"
+          icon={<FaListUl />}
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          colorScheme={editor.isActive("bulletList") ? "blue" : "gray"}
+        />
+        <TooltipIconButton
+          label="Numbered List"
+          variant={editor.isActive("orderedList") ? "solid" : "outline"}
+          size="xs"
+          aria-label="Numbered-List"
+          icon={<FaListOl />}
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          colorScheme={editor.isActive("orderedList") ? "blue" : "gray"}
+        />
+      </ButtonGroup>
+      <ButtonGroup isAttached>
+        <TooltipIconButton
+          label="Left Align"
+          variant={editor.isActive({ textAlign: "left" }) ? "solid" : "outline"}
+          size="xs"
+          aria-label="Justify-Text"
+          icon={<FaAlignLeft />}
+          onClick={() => editor.chain().focus().setTextAlign("left").run()}
+          colorScheme={editor.isActive({ textAlign: "left" }) ? "blue" : "gray"}
+        />
+        <TooltipIconButton
+          label="Justify"
+          variant={
+            editor.isActive({ textAlign: "justify" }) ? "solid" : "outline"
+          }
+          size="xs"
+          aria-label="Justify"
+          icon={<FaAlignJustify />}
+          onClick={() => editor.chain().focus().setTextAlign("justify").run()}
+          colorScheme={
+            editor.isActive({ textAlign: "justify" }) ? "blue" : "gray"
+          }
         />
       </ButtonGroup>
     </HStack>
