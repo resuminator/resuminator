044a16dc1c4d6ab5ee3f384a267aa0f6f210eea5
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-14 11:25:03 +0530

    Added Action Modal

---
+++ components/common/ActionModal.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@chakra-ui/button";
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/modal";
+import { ThemingProps } from "@chakra-ui/system";
+import React from "react";
+
+interface Props {
+  title: string;
+  buttonText: string;
+  color?: ThemingProps["colorScheme"];
+  onClose?: () => void;
+  onClick?: () => void;
+  isOpen?: boolean;
+}
+
+const ActionModal: React.FC<Props> = ({
+  title,
+  onClose,
+  isOpen,
+  onClick,
+  buttonText,
+  color = "red",
+  children,
+}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>{children}</ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme={color} mr={3} onClick={onClick}>
+            {buttonText}
+          </Button>
+          <Button mr={3} onClick={onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ActionModal;
