dd6b5c8fb9d7141aaae52763d402c2bc9aff9545
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-14 11:40:23 +0530

    Changed from Fragment to Box on Input

--- components/common/InputWithLabel.tsx
+++ components/common/InputWithLabel.tsx
@@ -1,18 +1,18 @@
 import { Input, InputProps } from "@chakra-ui/input";
-import { Text } from "@chakra-ui/layout";
-import React, { Fragment } from "react";
+import { Box, Text } from "@chakra-ui/layout";
+import React from "react";
 
 interface Props {
   label: string;
 }
 
 const InputWithLabel: React.FC<Props & InputProps> = ({ label, ...props }) => (
-  <Fragment>
+  <Box>
     <Text fontSize="md" pb="2" color="gray.500">
       {label}
     </Text>
     <Input variant="filled" shadow="sm" colorScheme="gray" mb="2" {...props} />
-  </Fragment>
+  </Box>
 );
 
 export default InputWithLabel;
