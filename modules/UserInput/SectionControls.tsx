c994d610082e21bba304cb6888fe6936e8a53800
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-20 18:15:08 +0530

    Added `extraChildren` prop to section controls

--- modules/UserInput/SectionControls.tsx
+++ modules/UserInput/SectionControls.tsx
@@ -1,5 +1,5 @@
 import { HStack } from "@chakra-ui/layout";
-import React from "react";
+import React, { ReactElement } from "react";
 import { FiEye, FiEyeOff, FiHelpCircle } from "react-icons/fi";
 import TooltipIconButton from "../../components/common/TooltipIconButton";
 
@@ -13,29 +13,34 @@ interface Props {
     toggleDisabled: () => void;
   };
   hasToggleButton?: boolean;
+  extraChildren?: ReactElement;
 }
 
 const SectionControls: React.FC<Props> = ({
   handler = { isDisabled: false, toggleDisabled: () => ({}) },
   hasToggleButton = true,
   children,
+  extraChildren,
 }) => {
   const { isDisabled, toggleDisabled } = handler;
   return (
     <HStack mb="4" spacing="4">
       {children}
-      {hasToggleButton && <TooltipIconButton
-        aria-label="Toggle Visibility"
-        label="Toggle Visibility"
-        icon={isDisabled ? <FiEyeOff /> : <FiEye />}
-        onClick={toggleDisabled}
-        colorScheme={isDisabled ? "red" : ""}
-      />}
+      {hasToggleButton && (
+        <TooltipIconButton
+          aria-label="Toggle Visibility"
+          label="Toggle Visibility"
+          icon={isDisabled ? <FiEyeOff /> : <FiEye />}
+          onClick={toggleDisabled}
+          colorScheme={isDisabled ? "red" : ""}
+        />
+      )}
       <TooltipIconButton
         aria-label="Know More"
         label="Know More"
         icon={<FiHelpCircle />}
       />
+      {extraChildren}
     </HStack>
   );
 };
