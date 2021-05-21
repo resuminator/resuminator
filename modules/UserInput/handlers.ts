e65f9c3bf32a7af3b569a8337762416783d05b9e
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-21 21:26:13 +0530

    Added handler docs

--- modules/UserInput/handlers.ts
+++ modules/UserInput/handlers.ts
@@ -2,6 +2,12 @@ import { Content } from "@tiptap/core";
 import { DropResult } from "react-beautiful-dnd";
 import { UpdateAction } from "../../store/types";
 
+/**
+ * Handles the input element change for the current object using `key:value` pair
+ * @param e Event Object
+ * @param index Index of the current object.
+ * @param action Callback action to perform. TypeOf (index: number, key: string, value: any)
+ */
 export const handleChange = (
   e: React.ChangeEvent<HTMLInputElement>,
   index: number,
@@ -12,28 +18,58 @@ export const handleChange = (
   action(index, key, value);
 };
 
+/**
+ * Handle the change from an uncontrolled TipTap Editor instance in a controlled manner.
+ * @param index Index of the current object.
+ * @param output Output from the TipTap Editor instance. Typeof Content.
+ * @param action Callback action to perform. TypeOf `(index: number, key: string, value: any)`
+ */
 export const handleEditorChange = (
   index: number,
   output: Content,
   action: UpdateAction
-) => {
+): void => {
   action(index, "description", output);
 };
 
+/**
+ * Handles the change for MUI DatePicker component.
+ * @param index Index of the current object.
+ * @param key Key to update. Generally "start" or "end"
+ * @param action Callback action to perform. TypeOf `(index: number, key: string, value: any)`
+ * @returns
+ */
 export const handleDateChange =
   (index: number, key: string, action: UpdateAction) => (date: Date) => {
     action(index, key, date);
   };
 
+/**
+ * Toggles the checkbox by controlling the value of the `end` date.
+ * @param index Index of the current object.
+ * @param value Value of "end" or Key to update. Toggles between `null` and Present Date
+ * @param action Callback action to perform. TypeOf `(index: number, key: string, value: any)`
+ * @param key Optional argument to change the key for which the value needs to be updated. Default = "end"
+ * @returns void
+ */
 export const handlePresentCheckbox = (
   index: number,
-  key: Date,
-  action: UpdateAction
+  value: Date,
+  action: UpdateAction,
+  key?: string
 ) => {
-  if (key) return action(index, "end", null);
-  else return action(index, "end", new Date());
+  const K = key || "end";
+  if (value) return action(index, K, null);
+  else return action(index, K, new Date());
 };
 
+/**
+ * Uses string input separated by comma(',') and saves an array as tags.
+ * @param e Event Object
+ * @param index Index of the current object.
+ * @param action Callback action to perform. TypeOf `(index: number, key: string, value: any)`
+ * @param key Optional argument to change the key for which the value needs to be updated. Default = "tags"
+ */
 export const handleTagsInput = (
   e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   index: number,
@@ -46,6 +82,13 @@ export const handleTagsInput = (
   action(index, K, tags);
 };
 
+/**
+ * Handles the reordering of a list upon drag end.
+ * @param result DropResult from react-beautiful-dnd.
+ * @param data List to update and set as state.
+ * @param setData Callback Function to set the `data`. Typeof (data: Array<T>) => void
+ * @returns void | null
+ */
 export const handleDragEnd = <T>(
   result: DropResult,
   data: Array<T>,
