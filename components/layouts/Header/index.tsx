e1955ea8ed2ca50945a0499761d400f9b7ff1113
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-24 12:54:16 +0530

    Refactored Header

--- components/layouts/Header/index.tsx
+++ components/layouts/Header/index.tsx
@@ -2,8 +2,8 @@ import { Avatar } from "@chakra-ui/avatar";
 import { Box } from "@chakra-ui/layout";
 import { useRouter } from "next/router";
 import React from "react";
-import LogoSquare from "./LogoSquare";
-import NavTabs from "./NavTabs";
+import LogoSquare from "../LogoSquare";
+import NavTabs from "../NavTabs";
 
 const Header = () => {
   const router = useRouter();
