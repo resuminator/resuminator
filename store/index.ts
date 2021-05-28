d3cbd3039d0700f939c73c999d6f6e92a7638cc3
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-28 13:55:12 +0530

    Pre adding data

--- store/index.ts
+++ store/index.ts
@@ -13,7 +13,7 @@ const store = <ObjectType>(
   setData: (list) => set({ data: list }),
   //Adds a new object to array (a dummy function for now).
   //TODO: This will be converted to add new object on the server and then return and set the data state.
-  add: (obj) => set((state) => ({ data: [...state.data, obj] })),
+  add: (obj) => set((state) => ({ data: [obj, ...state.data] })),
   //Updates an object at a particular index with a key and value pair.
   update: (index, key, value) => {
     const obj = { ...get().data[index], [key]: value };
