89922f2ad40a8632395cd9c587f2617971519181
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-30 14:03:42 +0530

    Updated with Chrome-AWS-Lambda

--- pages/api/download.ts
+++ pages/api/download.ts
@@ -1,28 +1,64 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from "next";
-import puppeteer from "puppeteer";
+
+const getBrowserInstance = async () => {
+  const chromium = require("chrome-aws-lambda");
+  const executablePath = await chromium.executablePath;
+
+  if (!executablePath) {
+    const puppeteer = require("puppeteer");
+    return await puppeteer.launch({
+      args: chromium.args,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
+  }
+
+  return await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const browser = await puppeteer.launch({ headless: true,  args: ['--no-sandbox'] });
-  const page = await browser.newPage();
-
   const url = "https://resuminator-next.netlify.app/resume";
+  if (!url || !url.trim()) {
+    res.status(400).json({ message: "No valid url found" });
+    return;
+  }
 
-  await page.goto(url, {
-    waitUntil: "domcontentloaded",
-  });
+  let pdf = null;
+  let browser = null;
 
-  const pdf = await page.pdf({
-    printBackground: true,
-    format: "a4",
-    margin: {
-      top: "0px",
-      bottom: "0px",
-      left: "0px",
-      right: "0px",
-    },
-  });
+  try {
+    browser = await getBrowserInstance();
+    const page = await browser.newPage();
+
+    await page.goto(url, {
+      waitUntil: "domcontentloaded",
+    });
+
+    pdf = await page.pdf({
+      printBackground: true,
+      format: "a4",
+      margin: {
+        top: "0px",
+        bottom: "0px",
+        left: "0px",
+        right: "0px",
+      },
+    });
+  } catch (e) {
+    return res.status(500).json(e);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
 
   await browser.close();
   res.setHeader("Content-Type", "application/pdf");
