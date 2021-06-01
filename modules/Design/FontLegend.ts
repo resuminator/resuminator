527ee7008c7dac5f18d1d3865a9a1cee23be4cc0
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-06-01 23:36:10 +0530

    Added fonts legend

---
+++ modules/Design/FontLegend.ts
@@ -0,0 +1,36 @@
+import { FontsObject } from "./types";
+
+const Fonts: FontsObject = {
+  CLASSIC: {
+    family: {
+      primary: "Inter",
+      secondary: "Inter",
+    },
+  },
+  MAGAZINE: {
+    family: {
+      primary: "Playfair Display",
+      secondary: "Source Sans Pro",
+    },
+  },
+  POISE: {
+    family: {
+      primary: "Inter",
+      secondary: "Inter",
+    },
+  },
+  SENIOR: {
+    family: {
+      primary: "Inter",
+      secondary: "Inter",
+    },
+  },
+  SPACE: {
+    family: {
+      primary: "Inter",
+      secondary: "Inter",
+    },
+  },
+};
+
+export default Fonts;
