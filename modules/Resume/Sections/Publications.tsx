0f716dbd371eaec8be78fb989f10428fd0450d92
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-06-06 17:47:34 +0530

    Added Publication Layout

--- modules/Resume/Sections/Publications.tsx
+++ modules/Resume/Sections/Publications.tsx
@@ -1,9 +1,71 @@
-import React from "react";
+import { Box, Text } from "@chakra-ui/layout";
+import React, { Fragment } from "react";
+import DataRow from "../../../components/elements/DataRow";
+import usePublicationStore from "../../UserInput/Publications/store";
+import { PublicationDataObject } from "../../UserInput/Publications/types";
+import SectionContent from "../components/SectionContent";
+import SectionTitle from "../components/SectionTitle";
+import TextItem from "../components/TextItem";
 
-interface Props {}
+const WithQuotes = ({ text = "" }) =>
+  text.length ? (
+    <Text mx="1" as="span" fontStyle="italic">
+      &ldquo;{text}&rdquo;
+    </Text>
+  ) : null;
 
-const PublicationsLayout = (props: Props) => {
-  return <h2>Publications</h2>;
+  /**
+   * Appends and Prepends a string with special characters
+   * @param prefix String: Special Character
+   * @param text String: Text to wrap
+   * @param suffix String: Special Character
+   * @returns Null if text is not present, {prefix}{text}{suffix} in all other cases
+   */
+const withSpecialCharacters = (prefix = "", text = "", suffix = "") =>
+  text.length ? `${prefix}${text}${suffix}` : null;
+
+const MLAStyle: React.FC<{ item: PublicationDataObject }> = ({ item }) => (
+  <Fragment>
+    {item.authorNames} <WithQuotes text={item.articleTitle} />{" "}
+    {item.journalName} {withSpecialCharacters("", item.volumeNumber, ".")}
+    {item.issueNumber} {withSpecialCharacters("(", item.year, ")")}{" "}
+    {withSpecialCharacters(": ", item.pages)}
+  </Fragment>
+);
+
+const APAStyle: React.FC<{ item: PublicationDataObject }> = ({ item }) => (
+  <Fragment>
+    {withSpecialCharacters("", item.authorNames, ".")}{" "}
+    {withSpecialCharacters("", item.articleTitle, ".")} {item.journalName}{" "}
+    {item.volumeNumber}
+    {withSpecialCharacters("(", item.issueNumber, "): ")}
+    {withSpecialCharacters("", item.pages, ". ")} {item.year}
+  </Fragment>
+);
+
+const PublicationsLayout = () => {
+  const data = usePublicationStore((state) => state.data).filter(
+    (item) => !item.isHidden
+  );
+
+  return (
+    <Box aria-label="Publication Layout" w="inherit">
+      <SectionTitle>Publications</SectionTitle>
+      <SectionContent>
+        {data.map((item) => (
+          <DataRow mb="1" key={item._id}>
+            <TextItem as="li">
+              {item.format === "MLA" ? (
+                <MLAStyle item={item} />
+              ) : (
+                <APAStyle item={item} />
+              )}
+            </TextItem>
+          </DataRow>
+        ))}
+      </SectionContent>
+    </Box>
+  );
 };
 
 export default PublicationsLayout;
