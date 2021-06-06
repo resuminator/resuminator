6eee7d5a0e25e0b6d7e752b1f664fb397dbde957
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-06-06 13:32:49 +0530

    Styled Tags

--- modules/Resume/components/Tags.tsx
+++ modules/Resume/components/Tags.tsx
@@ -1,7 +1,8 @@
 import { Box } from "@chakra-ui/layout";
 import { Tag } from "@chakra-ui/tag";
-import React from "react";
+import React, { useContext } from "react";
 import useResumeStore from "../../../store/resume.store";
+import { StylePropsContext } from "../../Design/StylePropsProvider";
 
 interface Props {
   list: Array<string>;
@@ -9,11 +10,20 @@ interface Props {
 
 const Tags: React.FC<Props> = ({ list }) => {
   const color = useResumeStore((state) => state.color);
+  const font = useContext(StylePropsContext).font;
 
   return (
     <Box>
       {list.map((tag) => (
-        <Tag colorScheme={color} variant="subtle" key={tag} mr="1">
+        <Tag
+          colorScheme={color}
+          {...font.body}
+          borderRadius="full"
+          variant="subtle"
+          key={tag}
+          mr="1"
+          mb="1.5"
+        >
           {tag}
         </Tag>
       ))}
