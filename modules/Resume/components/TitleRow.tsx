f6625ae76fe1dbbc9ecde17905ca944406442dd3
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-06-06 12:21:40 +0530

    Added hover props on TitleRow

--- modules/Resume/components/TitleRow.tsx
+++ modules/Resume/components/TitleRow.tsx
@@ -1,10 +1,27 @@
+import Icon from "@chakra-ui/icon";
 import { Text, TextProps } from "@chakra-ui/layout";
+import { ComponentWithAs } from "@chakra-ui/system";
 import React, { useContext } from "react";
+import { FiExternalLink } from "react-icons/fi";
 import { StylePropsContext } from "../../Design/StylePropsProvider";
 
-const TitleRow: React.FC<TextProps> = ({ children, ...props }) => {
+const TitleRow: ComponentWithAs<"p", TextProps> = ({ children, ...props }) => {
   const titleRowProps = useContext(StylePropsContext).titleRowProps;
-  return <Text {...titleRowProps} {...props}>{children}</Text>;
+  const hoverProps = props.href && {
+    _hover: { textDecoration: "underline" },
+    transition: "0.2s all",
+  };
+  
+  return (
+    <Text {...titleRowProps} {...props} {...hoverProps}>
+      {children}
+      {props.href && (
+        <span>
+          <Icon as={FiExternalLink} mx="1" height="0.8rem" />
+        </span>
+      )}
+    </Text>
+  );
 };
 
 export default TitleRow;
