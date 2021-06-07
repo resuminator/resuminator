d38135cb54f79cda5308ba741d0b45ebc65f9a7c
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-06-07 17:08:58 +0530

    Added Social Handles

--- modules/Resume/Header/SocialHandles.tsx
+++ modules/Resume/Header/SocialHandles.tsx
@@ -1,9 +1,36 @@
+import Icon from "@chakra-ui/icon";
+import { Box, HStack } from "@chakra-ui/layout";
 import React from "react";
+import {
+  getColorSchemeForService,
+  getIconForService
+} from "../../UserInput/Contact/helpers";
+import useContactStore from "../../UserInput/Contact/store";
+import TextItem from "../components/TextItem";
 
-interface Props {}
+const SocialHandlesLayout = () => {
+  const data = useContactStore((state) => state.contact);
 
-const SocialHandlesLayout = (props: Props) => {
-  return <h2>SocialHandles</h2>;
+  return (
+    <HStack wrap="wrap">
+      {data
+        .filter((item) => !item.isHidden)
+        .map((item) => (
+          <Box display="flex" alignItems="center" key={item.label}>
+            <Icon
+              as={getIconForService(item.label)}
+              color={getColorSchemeForService(item.label)}
+              mx="1"
+            />
+            {item.identifier ? (
+              <TextItem>{item.identifier}</TextItem>
+            ) : (
+              <TextItem>{item.link}</TextItem>
+            )}
+          </Box>
+        ))}
+    </HStack>
+  );
 };
 
 export default SocialHandlesLayout;
