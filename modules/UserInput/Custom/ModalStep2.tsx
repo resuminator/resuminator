524521a0739745a2f48da54cb94e33b7998ffdf2
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-06-15 13:50:54 +0530

    Updated Preview Styling

--- modules/UserInput/Custom/ModalStep2.tsx
+++ modules/UserInput/Custom/ModalStep2.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import React, { Fragment } from "react";
 import DataRow from "../../../components/elements/DataRow";
 import { CustomSectionObject } from "../../../store/types";
@@ -20,18 +20,32 @@ const ModalStep2: React.FC<Props> = ({ section }) => {
         Arrange the layout of the selected inputs on your resume. Drag the items
         to rearrange the layout.
       </Text>
-      <Text mb="2" fontWeight="semibold" fontSize="lg">
-        {section.header}
+      <Text
+        mb="1"
+        textTransform="uppercase"
+        fontWeight="semibold"
+        fontSize="xs"
+        color="gray.400"
+        letterSpacing="wider"
+      >
+        Preview
       </Text>
-      <SectionContent>
-        {layout.map((row, index) => (
-          <DataRow key={index}>
-            {row.map((sectionId) => (
-              <Text key={sectionId}>{getInputFieldFromId(sectionId).name}</Text>
-            ))}
-          </DataRow>
-        ))}
-      </SectionContent>
+      <Box m="4" p="4" border="2px" borderColor="gray.200" borderRadius="10px">
+        <Text mb="2" color="purple.600" fontWeight="semibold" fontSize="lg">
+          {section.header}
+        </Text>
+        <SectionContent>
+          {layout.map((row, index) => (
+            <DataRow key={index}>
+              {row.map((sectionId) => (
+                <Text key={sectionId}>
+                  {getInputFieldFromId(sectionId).name}
+                </Text>
+              ))}
+            </DataRow>
+          ))}
+        </SectionContent>
+      </Box>
     </Fragment>
   );
 };
