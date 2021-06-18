87de84accf97649cd7586b8a8de6b6070a3a9a12
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-06-18 18:55:54 +0530

    Displaying Custom Sections

--- modules/UserInput/Custom/CustomSectionInputs.tsx
+++ modules/UserInput/Custom/CustomSectionInputs.tsx
@@ -1,37 +1,24 @@
-import { Button, ButtonGroup, useDisclosure } from "@chakra-ui/react";
-import React from "react";
-import { FiPlus } from "react-icons/fi";
-import { RiListSettingsFill } from "react-icons/ri";
+import { useDisclosure } from "@chakra-ui/react";
+import React, { Fragment } from "react";
 import Section from "../../../components/layouts/Section";
-import NewSectionModal from "./NewSectionModal";
 import { useCustomSectionStore } from "./store";
 
 const CustomSectionInputs = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const data = useCustomSectionStore(state => state.data);
+  const customSections = useCustomSectionStore((state) => state.data);
 
   return (
-    <Section
-      header={{
-        title: "Custom Sections",
-        subtitle: "Create and manage your custom sections for this resume",
-        mb: "2",
-      }}
-    >
-      <ButtonGroup my="2">
-        <Button colorScheme="purple" leftIcon={<FiPlus />} onClick={onOpen}>
-          Create new section
-        </Button>
-        <Button
-          colorScheme="purple"
-          variant="ghost"
-          leftIcon={<RiListSettingsFill />}
-        >
-          Manage custom sections
-        </Button>
-      </ButtonGroup>
-      <NewSectionModal isOpen={isOpen} onClose={onClose} />
-    </Section>
+    <Fragment>
+      {customSections.map((section) => (
+        <Section
+          key={section.header}
+          header={{
+            title: section.header,
+            mb: "2",
+          }}
+        ></Section>
+      ))}
+    </Fragment>
   );
 };
 
