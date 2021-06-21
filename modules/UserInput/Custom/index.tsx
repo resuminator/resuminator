49ff7452f995e3adbd47016a3d3b28a7b6a30dd8
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-06-21 16:58:20 +0530

    Added Custom Section Limit

--- modules/UserInput/Custom/index.tsx
+++ modules/UserInput/Custom/index.tsx
@@ -1,12 +1,30 @@
-import { Button, ButtonGroup, useDisclosure } from "@chakra-ui/react";
+import { Button, ButtonGroup, useDisclosure, useToast } from "@chakra-ui/react";
 import React from "react";
 import { FiPlus } from "react-icons/fi";
 import { RiListSettingsFill } from "react-icons/ri";
 import Section from "../../../components/layouts/Section";
 import NewSectionModal from "./NewSectionModal";
+import { useCustomSectionStore } from "./store";
 
 const CustomSections = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const customSections = useCustomSectionStore((state) => state.sections);
+  const toast = useToast();
+
+  const handleCreateButton = () => {
+    if (customSections.length < 3) {
+      onOpen();
+    } else {
+      return toast({
+        title: "Maximum Limit Reached!",
+        description:
+          "You can add a maximum of 3 custom sections. Try modifying or deleting existing sections.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
     <Section
@@ -17,7 +35,11 @@ const CustomSections = () => {
       }}
     >
       <ButtonGroup my="2">
-        <Button colorScheme="purple" leftIcon={<FiPlus />} onClick={onOpen}>
+        <Button
+          colorScheme="purple"
+          leftIcon={<FiPlus />}
+          onClick={handleCreateButton}
+        >
           Create new section
         </Button>
         <Button
