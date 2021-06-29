d6c208f6bccd2174c061b6feacfcda5594de51fb
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-06-29 13:57:53 +0530

    Updated BoxHeader Props

--- components/common/BoxHeader.tsx
+++ components/common/BoxHeader.tsx
@@ -1,4 +1,5 @@
 import { Box, BoxProps, Text } from "@chakra-ui/layout";
+import { TextProps } from "@chakra-ui/react";
 import React from "react";
 
 export interface BoxHeaderProps {
@@ -6,6 +7,7 @@ export interface BoxHeaderProps {
   subtitle?: string;
   size?: { title: string; subtitle?: string };
   spacing?: string;
+  titleProps?: TextProps;
 }
 
 const BoxHeader: React.FC<BoxHeaderProps & BoxProps> = ({
@@ -13,11 +15,12 @@ const BoxHeader: React.FC<BoxHeaderProps & BoxProps> = ({
   subtitle,
   size = { title: "2xl", subtitle: "md" },
   spacing = "0.5",
+  titleProps,
   ...rest
 }) => {
   return (
     <Box mb="8" {...rest}>
-      <Text fontSize={size.title} fontWeight="semibold">
+      <Text {...titleProps} fontSize={size.title} fontWeight="semibold">
         {title}
       </Text>
       <Box p={spacing}/>
