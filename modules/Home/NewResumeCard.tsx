20f8e685696998a25ffc38f2fbcb4463d892dafc
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-06-30 18:08:26 +0530

    Added hover props to new resume card

--- modules/Home/NewResumeCard.tsx
+++ modules/Home/NewResumeCard.tsx
@@ -1,19 +1,26 @@
-import { Center, Icon, Text } from "@chakra-ui/react";
+import { Center, Icon, Text, useColorModeValue } from "@chakra-ui/react";
 import { FiPlusCircle } from "react-icons/fi";
 
 const NewResumeCard = () => {
+  const hoverProps = {
+    bg: useColorModeValue("gray.100", "whiteAlpha.100"),
+    color: useColorModeValue("blue.500", "blue.300"),
+  };
+
   return (
     <Center
       flexDir="column"
       h="10rem"
       w="10rem"
       border="1px solid"
-      borderColor="GrayText"
       borderRadius="10px"
       cursor="pointer"
+      color={useColorModeValue("gray.600", "whiteAlpha.600")}
+      _hover={hoverProps}
+      transition="0.2s all"
     >
-      <Icon as={FiPlusCircle} w={6} h={6} color="Highlight" />
-      <Text my="2" color="Highlight" fontWeight="semibold" fontSize="sm">
+      <Icon as={FiPlusCircle} w={6} h={6} />
+      <Text my="2" fontWeight="semibold" fontSize="sm">
         Create New Resume
       </Text>
     </Center>
