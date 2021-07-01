36e717ea48462ae582354e2d9d7b6b3ff2d5f430
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-01 16:33:09 +0530

    Removing persist from resume store

--- store/resume.store.ts
+++ store/resume.store.ts
@@ -5,49 +5,46 @@ import { ResumeStore } from "./types";
 
 const useResumeStore = create<ResumeStore>(
   devtools(
-    persist(
-      (set) => ({
-        _id: "",
-        profileName: "",
-        icon: "",
-        privacy: { isPublic: false, isClonable: false },
-        properties: {
-          inputs: [],
-          layout: { header: [[]], body: [[]], footer: [] },
-        },
-        fontProfile: "CLASSIC",
-        spacing: 1,
-        color: "blue",
-        togglePrivacy: (key: "isPublic" | "isClonable") =>
-          set((state) => ({
-            privacy: { ...state.privacy, [key]: !state.privacy[key] },
-          })),
-        updateInputs: (arr) =>
-          set((state) => ({
-            properties: {
-              ...state.properties,
-              inputs: arr,
-            },
-          })),
-        updateLayout: (key: string, value: any) =>
-          set((state) => ({
-            properties: {
-              ...state.properties,
-              layout: { ...state.properties.layout, [key]: value },
-            },
-          })),
-        setProperty: (key, value) =>
-          set((state) =>
-            produce(state, (draftState) => {
-              draftState[key] = value;
-            })
-          ),
-        setColorProfile: (value) => set({color: value}),
-        setFontProfile: (value) => set({fontProfile: value}),
-        setSpacing: (value) => set({spacing: value})
-      }),
-      { name: "current-resume", getStorage: () => sessionStorage }
-    ),
+    (set) => ({
+      _id: "",
+      profileName: "",
+      icon: "",
+      privacy: { isPublic: false, isClonable: false },
+      properties: {
+        inputs: [],
+        layout: { header: [[]], body: [[]], footer: [] },
+      },
+      fontProfile: "CLASSIC",
+      spacing: 1,
+      color: "blue",
+      togglePrivacy: (key: "isPublic" | "isClonable") =>
+        set((state) => ({
+          privacy: { ...state.privacy, [key]: !state.privacy[key] },
+        })),
+      updateInputs: (arr) =>
+        set((state) => ({
+          properties: {
+            ...state.properties,
+            inputs: arr,
+          },
+        })),
+      updateLayout: (key: string, value: any) =>
+        set((state) => ({
+          properties: {
+            ...state.properties,
+            layout: { ...state.properties.layout, [key]: value },
+          },
+        })),
+      setProperty: (key, value) =>
+        set((state) =>
+          produce(state, (draftState) => {
+            draftState[key] = value;
+          })
+        ),
+      setColorProfile: (value) => set({ color: value }),
+      setFontProfile: (value) => set({ fontProfile: value }),
+      setSpacing: (value) => set({ spacing: value }),
+    }),
     "Resume"
   )
 );
