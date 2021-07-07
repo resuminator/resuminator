4aee930250f0d6bb7845719463698f2b1e5da5bb
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-07 12:50:51 +0530

    Added Settings Sidebar

--- modules/Home/SidebarSection.tsx
+++ modules/Home/SidebarSection.tsx
@@ -1,21 +1,33 @@
-import { Box, Button, HStack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ButtonProps,
+  HStack, Text
+} from "@chakra-ui/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { IconType } from "react-icons/lib";
 
+export type SideBarItems = Array<{
+  title: string;
+  icon: IconType;
+  link: string;
+}>;
 interface SidebarOptionsProps {
-  sectionTitle: string;
-  items: Array<{
-    title: string;
-    icon: IconType;
-    link: string;
-  }>;
+  sectionTitle?: string;
+  items: SideBarItems;
+  itemProps?: ButtonProps;
 }
 
 const SidebarSection: React.FC<SidebarOptionsProps> = ({
   sectionTitle,
   items,
+  itemProps = { size: "sm" },
 }) => {
+  const router = useRouter();
+  const pathname = router.pathname;
+  
   return (
     <Box mb="4" width="100%">
       <Text
@@ -29,16 +41,21 @@ const SidebarSection: React.FC<SidebarOptionsProps> = ({
         {sectionTitle}
       </Text>
       {items.map((item) => (
-        <HStack key={item.title} transition="0.2s all" width="inherit">
+        <HStack
+          key={item.title}
+          transition="0.2s all"
+          width="inherit"
+        >
           <Link href={item.link}>
             <Button
+              {...itemProps}
               isFullWidth
               isDisabled={!item.link.length}
               variant="ghost"
               justifyContent="left"
-              fontWeight="normal"
-              size="sm"
+              fontWeight={item.link === pathname ? "semibold" : "normal"}
               leftIcon={<item.icon />}
+              color={item.link === pathname ? "blue" : "inherit"}
             >
               {item.title}
             </Button>
---
+++ modules/Settings/SettingsSidebar.tsx
@@ -0,0 +1,70 @@
+import { Box, GridItem, useColorModeValue, VStack } from "@chakra-ui/react";
+import React from "react";
+import {
+    FiAlertCircle,
+    FiCheckCircle,
+    FiDatabase,
+    FiFileText,
+    FiUser
+} from "react-icons/fi";
+import SidebarSection, { SideBarItems } from "../Home/SidebarSection";
+
+const SettingsSidebar = () => {
+  const general: SideBarItems = [
+    {
+      title: "General",
+      icon: FiUser,
+      link: "/settings",
+    },
+    {
+      title: "Preferences",
+      icon: FiCheckCircle,
+      link: "/settings/preferences",
+    },
+    {
+      title: "Resume",
+      icon: FiFileText,
+      link: "/settings/resume",
+    },
+    {
+      title: "Data & Security",
+      icon: FiDatabase,
+      link: "/settings/data",
+    },
+    {
+      title: "Advanced",
+      icon: FiAlertCircle,
+      link: "/settings/advanced",
+    },
+  ];
+  return (
+    <GridItem rowSpan={5} colSpan={1}>
+      <Box
+        alignItems="flex-start"
+        flexDir="column"
+        display="flex"
+        height="100%"
+        width="100%"
+        aria-label="section"
+        px={{ base: "1rem" }}
+      >
+        <VStack
+          alignItems="flex-start"
+          spacing="0"
+          color={useColorModeValue("gray.600", "whiteAlpha")}
+          width="100%"
+          mb="4"
+        >
+          <SidebarSection
+            items={general}
+            itemProps={{
+              size: "md",
+            }}
+          />
+        </VStack>
+      </Box>
+    </GridItem>
+  );
+};
+
+export default SettingsSidebar;
