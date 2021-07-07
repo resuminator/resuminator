8c13b2bbc185485b4b6840cbcdc6f4faaafa8ef8
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-07 20:19:55 +0530

    Added Request Data

--- modules/Settings/Security/RequestData.tsx
+++ modules/Settings/Security/RequestData.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, GridItem, Text } from "@chakra-ui/react";
+import { Box, Button, Divider, GridItem, Text } from "@chakra-ui/react";
 import React from "react";
 import BoxHeader from "../../../components/common/BoxHeader";
 import useUserStore from "../../User/store";
@@ -13,7 +13,13 @@ const RequestData = () => {
           size={{ title: "lg", subtitle: "sm" }}
           mb="2.5"
         />
-
+        <Text fontSize="md" mb="4">
+          You can request a copy of all the data you have ever stored in
+          Resuminator including your personal information, data entered in any
+          resume, and all metadata for your account linked to {email} by raising
+          a request.
+        </Text>
+        <Button colorScheme="purple" size="sm" mb="4">Request Account Data</Button>
         <Text fontSize="sm" color="gray" mb="2">
           Your data will be sent to you via your primary email {`(${email})`}{" "}
           within 14 days of request.
