c1db4e374af6f8bf7ac3f7a7f8a71cba1c8c6aee
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-07 12:57:49 +0530

    Added Settings Pages

---
+++ pages/settings/preferences.tsx
@@ -0,0 +1,59 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+import { NextPage } from "next";
+import React from "react";
+import { QueryClient, useQuery } from "react-query";
+import { dehydrate } from "react-query/hydration";
+import getUserData from "../../apis/getUserData";
+import BoxHeader from "../../components/common/BoxHeader";
+import Footer from "../../components/layouts/Footer";
+import Header from "../../components/layouts/Header";
+import { userPlaceholder } from "../../data/placeholderData";
+import SettingsSidebar from "../../modules/Settings/SettingsSidebar";
+import { UserObject } from "../../modules/User/types";
+import InitUserStore from "../../store/InitUserStore";
+
+const Preferences: NextPage = () => {
+  const { data, status } = useQuery<UserObject, Error>(
+    "getUserData",
+    getUserData,
+    { placeholderData: userPlaceholder }
+  );
+
+  return (
+    <>
+      <InitUserStore data={data} status={status} />
+      <Header />
+      <Grid
+        height="100vh"
+        templateRows="repeat(6, 1fr)"
+        templateColumns="repeat(4, 1fr)"
+        gap={4}
+        mx={{ md: "4rem", lg: "7rem" }}
+        my={{ base: "2rem" }}
+      >
+        <GridItem rowSpan={1} colSpan={4}>
+          <BoxHeader
+            title="Settings"
+            subtitle="Select a section from the sidebar to change its settings"
+          />
+        </GridItem>
+        <SettingsSidebar />
+      </Grid>
+      <Footer />
+    </>
+  );
+};
+
+export default Preferences;
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery("getUserData", getUserData);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
