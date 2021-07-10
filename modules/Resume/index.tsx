e0dd024ebb0a355f2790af7ba1866a086b92ade6
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-10 11:40:47 +0530

    Implemented Drop in Sections

--- modules/Resume/index.tsx
+++ modules/Resume/index.tsx
@@ -1,13 +1,12 @@
 import { Box } from "@chakra-ui/layout";
+import produce from "immer";
 import React from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
 import ColoredDivider from "../../components/common/ColoredDivider";
 import useResumeStore from "../../store/resume.store";
 import { isCustom } from "../Design/Colors/ColorSelector";
 import StylePropsProvider from "../Design/StylePropsProvider";
 import BodyBox from "./components/BodyBox";
-import BodyColumn from "./components/BodyColumn";
-import BodySectionBox from "./components/BodySectionBox";
 import HeaderBox from "./components/HeaderBox";
 import HeaderRow from "./components/HeaderRow";
 import Paper from "./components/Paper";
@@ -15,9 +14,39 @@ import { getHeaderLayout, getLayout } from "./legend";
 
 const ResumePaper = () => {
   const { header, body } = useResumeStore((state) => state.properties.layout);
+  const updateLayout = useResumeStore((state) => state.updateLayout);
   const spacing = useResumeStore((state) => state.spacing);
   const color = useResumeStore((state) => state.color);
   const primaryColor = isCustom(color) ? color : `${color}.600`;
+
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
+    if (!destination) return;
+
+    //If the column is same then create a new array and replace the original one.
+    if (source.droppableId === destination.droppableId) {
+      const parts = Array.from(body[source.droppableId]);
+      const [reorderedPart] = parts.splice(source.index, 1);
+      parts.splice(destination.index, 0, reorderedPart);
+      
+      const nextBody = produce(body, draft => {
+        draft[source.droppableId] = parts;
+      })
+      updateLayout("body", nextBody)
+    } else {
+      const sourceParts = Array.from(body[source.droppableId]);
+      const destinationParts = Array.from(body[destination.droppableId]);
+      const [reorderedPart] = sourceParts.splice(source.index, 1);
+      destinationParts.splice(destination.index, 0, reorderedPart);
+
+      const nextBody = produce(body, draft => {
+        draft[source.droppableId] = sourceParts;
+        draft[destination.droppableId] = destinationParts;
+      })
+
+      updateLayout("body", nextBody)
+    }
+  };
 
   return (
     <StylePropsProvider>
@@ -34,10 +63,11 @@ const ResumePaper = () => {
           ))}
         </HeaderBox>
         <ColoredDivider color={primaryColor} mb="2" />
-        <DragDropContext onDragEnd={() => console.log("Drag Complete")}>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
           <BodyBox py={spacing * 2}>
             {body.map((rowAsColumn, index) => (
-              <Droppable key={index} droppableId={`column-${index + 1}`}>
+              <Droppable key={index} droppableId={`${index}`}>
+                {/*The 'provided' argument gives props to make columns as droppable areas*/}
                 {(provided) => (
                   <Box
                     display="flex"
@@ -57,6 +87,7 @@ const ResumePaper = () => {
                         width="100%"
                         key={layoutKey}
                       >
+                        {/*These extra props are drilled to SectionBox component to enable dragging*/}
                         {getLayout(layoutKey, {
                           draggableId: layoutKey,
                           index: index,
