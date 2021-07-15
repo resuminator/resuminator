79d90eaee1713a0f6f5b195300c392a322ac956e
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-15 15:52:44 +0530

    Added Firebase Client Service

---
+++ services/firebase/index.ts
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
+
+if (typeof window !== "undefined" && !firebase.apps.length) {
+  try {
+    firebase.initializeApp(firebaseConfig);
+  } catch (err) {
+    if (!/already exists/.test(err.message)) {
+      throw new Error(err);
+    }
+  }
+}
+const firebaseSDK = firebase;
+export default firebaseSDK;
