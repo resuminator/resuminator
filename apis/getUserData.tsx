e01d44d9502cfc9234609974585893cf27b8a39b
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-23 11:54:03 +0530

    Passing Token as Header

--- apis/getUserData.tsx
+++ apis/getUserData.tsx
@@ -1,8 +1,11 @@
 import axios from "axios";
-import API_URL from "../config/server";
+import API_URL, { API_VERSION } from "../config/server";
+import getHeaders from "../utils/headers";
 
-const getUserData = async () => {
-  const res = await axios.get(`${API_URL}/users`);
+const getUserData = async (token: string = null) => {
+  const res = await axios.get(`${API_URL}/${API_VERSION}/meta`, {
+    headers: getHeaders(token),
+  });
   return res.data;
 };
 
--- config/server.ts
+++ config/server.ts
@@ -1,3 +1,8 @@
-const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_API_BASE_URL
+    : "http://localhost:8080";
 
-export default API_URL
+export const API_VERSION = "v0.2.0"
+
+export default API_URL;
