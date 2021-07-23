44cd1c8103af62e00a55f70247529245470b7b97
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-23 18:35:30 +0530

    Integrated GET resume

--- apis/getResumeData.tsx
+++ apis/getResumeData.tsx
@@ -1,8 +1,11 @@
 import axios from "axios";
-import API_URL from "../config/server";
+import API_URL, { API_VERSION } from "../config/server";
+import getHeaders from "../utils/headers";
 
-const getResumeData = async (id: string) => {
-  const res = await axios.get(`${API_URL}/resume/${id}`);
+const getResumeData = async (token: string = null, _id: string) => {
+  const res = await axios.get(`${API_URL}/${API_VERSION}/resume/${_id}`, {
+    headers: getHeaders(token),
+  });
   return res.data;
 };
 
--- pages/create/[id].tsx
+++ pages/create/[id].tsx
@@ -1,6 +1,7 @@
 import { Box } from "@chakra-ui/layout";
-import { NextPage } from "next";
-import React, { Fragment } from "react";
+import { GetServerSidePropsContext, NextPage } from "next";
+import nookies from "nookies";
+import React, { Fragment, useEffect } from "react";
 import { QueryClient, useQuery } from "react-query";
 import { dehydrate } from "react-query/hydration";
 import getResumeData from "../../apis/getResumeData";
@@ -16,7 +17,7 @@ import {
   NameAndJobTitle,
   Projects,
   Publications,
-  Skills
+  Skills,
 } from "../../modules/UserInput";
 import UserImage from "../../modules/UserInput/Contact/UserImage";
 import CustomSections from "../../modules/UserInput/Custom";
@@ -45,20 +46,29 @@ const getInputSection = (key: InputSectionKeys) => {
 };
 
 interface CreateProps {
+  token: string;
   id: string;
 }
 
-const Create: NextPage<CreateProps> = ({ id }) => {
+const Create: NextPage<CreateProps> = ({ token, id }) => {
   const inputs = useResumeStore((state) => state.properties.inputs);
-  const { data, status } = useQuery("getResumeData", () => getResumeData(id), {
-    placeholderData,
-  });
+  const { data, status } = useQuery(
+    "getResumeData",
+    () => getResumeData(token, id),
+    {
+      placeholderData,
+    }
+  );
   const { data: userData, status: userQueryStatus } = useQuery<
     UserObject,
     Error
-  >("getUserData", getUserData, {
+  >("getUserData", () => getUserData(token), {
     placeholderData: userPlaceholder,
   });
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   return (
     <>
@@ -99,14 +109,30 @@ const Create: NextPage<CreateProps> = ({ id }) => {
 
 export default Create;
 
-export const getServerSideProps = async (context) => {
-  const { id } = context.params;
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  //Try to get token from cookies.
+  const cookies = nookies.get(ctx);
+  const token = cookies.token;
+
+  //If the token does not exist or is cleared then redirect to login page.
+  if (!token) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/login",
+      },
+    };
+  }
+
+  //If token is present, pass it to the query to fetch data from API
+  const { id } = ctx.params;
   const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery("getResumeData", () => getResumeData(id));
-
+  await queryClient.prefetchQuery("getResumeData", () =>
+    getResumeData(token, id.toString())
+  );
   return {
     props: {
+      token,
       id,
       dehydratedState: dehydrate(queryClient),
     },
