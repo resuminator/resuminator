8dc2558a3d7a974970da00f8dfa6ea0333b4b3a5
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-26 17:05:44 +0530

    Passing last saved as props

--- modules/Viewer/AutoSaveStatus.tsx
+++ modules/Viewer/AutoSaveStatus.tsx
@@ -30,34 +30,32 @@ const SavingChanges = () => (
   </HStack>
 );
 
-const Error = () => {
-  const { lastSavedAt } = useGlobalStore();
-  const diff = TimeDiff(lastSavedAt);
-
-  const text = parseInt(diff.split(" ")[0]) <= 0 ? "just now" : diff;
+const Error = ({ timeDiff }) => {
   return (
     <HStack>
       <FiAlertTriangle color="gray" />
-      <Message message={`Couldn't save last changes. Last saved ${text}`} />
+      <Message message={`Couldn't save last changes. Last saved ${timeDiff}`} />
     </HStack>
   );
 };
 
-const LastSaved = () => {
-  const { lastSavedAt } = useGlobalStore();
-  const diff = TimeDiff(lastSavedAt);
-
-  const text = parseInt(diff.split(" ")[0]) <= 0 ? "just now" : diff;
+const LastSaved = ({ timeDiff }) => {
   return (
     <HStack>
       <FiCheck color="gray" />
-      <Message message={`Changes saved ${text}`} />
+      <Message message={`Changes saved ${timeDiff}`} />
     </HStack>
   );
 };
 
 const AutoSaveStatus = (props: Props) => {
   const saveStatus = useGlobalStore((state) => state.saveStatus);
+  const { lastSavedAt } = useGlobalStore();
+  const diff = TimeDiff(lastSavedAt);
+
+  const JUST_NOW = 10;
+
+  const text = parseInt(diff.split(" ")[0]) <= JUST_NOW ? "just now" : diff;
 
   switch (saveStatus) {
     case Status.idle:
@@ -65,9 +63,9 @@ const AutoSaveStatus = (props: Props) => {
     case Status.loading:
       return <SavingChanges />;
     case Status.error:
-      return <Error />;
+      return <Error timeDiff={text} />;
     case Status.success:
-      return <LastSaved />;
+      return <LastSaved timeDiff={text} />;
   }
 };
 
