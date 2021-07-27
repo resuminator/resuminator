e9816be6e7ba63617d657851d28e412d063c52ff
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-27 13:47:29 +0530

    Patching Skills Data and Format

--- apis/patchSkills.tsx
+++ apis/patchSkills.tsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import API_URL, { API_VERSION } from "../config/server";
-import { CertificationDataObject } from "../modules/UserInput/Certification/types";
+import { SkillDataObject } from "../modules/UserInput/Skills/types";
 import getHeaders from "../utils/headers";
 
 export const patchSkillData = async (
   token: string = null,
   resumeId: string,
-  body: Array<CertificationDataObject>
+  body: Array<SkillDataObject>
 ) => {
   const res = await axios.patch(
     `${API_URL}/${API_VERSION}/resume/skill/data/${resumeId}`,
@@ -21,7 +21,7 @@ export const patchSkillData = async (
 export const patchSkillFormat = async (
   token: string = null,
   resumeId: string,
-  body: Array<CertificationDataObject>
+  body: { [key: string]: "CATEGORIES" | "TAGS" }
 ) => {
   const res = await axios.patch(
     `${API_URL}/${API_VERSION}/resume/skill/format/${resumeId}`,
--- modules/UserInput/Skills/FormatMenu.tsx
+++ modules/UserInput/Skills/FormatMenu.tsx
@@ -5,11 +5,32 @@ import React from "react";
 import { AiFillTags } from "react-icons/ai";
 import { BsGridFill } from "react-icons/bs";
 import { FaChevronDown } from "react-icons/fa";
+import { patchSkillFormat } from "../../../apis/patchSkills";
+import { useCustomToast } from "../../../hooks/useCustomToast";
+import { usePatchParams } from "../../../hooks/usePatchParams";
+import { Result } from "../../../store/types";
 import useSkillStore from "./store";
 
 const FormatMenu = () => {
-const format = useSkillStore(state => state.format); 
-const toggleFormat = useSkillStore((state) => state.toggleFormat);
+  const { format, setFormat } = useSkillStore();
+  const { token, resumeId } = usePatchParams();
+  const { createToast } = useCustomToast();
+
+  const handleSubmit = async () => {
+    return await patchSkillFormat(token, resumeId, { format: format === "CATEGORIES" ? "TAGS" : "CATEGORIES" })
+      .then((res: Result) => {
+        setFormat(res.skills.format);
+        return createToast("Skill display style updated", "success");
+      })
+      .catch(() =>
+        createToast(
+          "Couldn't update display style",
+          "error",
+          "Please try again in sometime"
+        )
+      );
+  };
+
   return (
     <Menu>
       <MenuButton
@@ -31,10 +52,10 @@ const toggleFormat = useSkillStore((state) => state.toggleFormat);
         )}
       </MenuButton>
       <MenuList>
-        <MenuItem icon={<BsGridFill />} onClick={() => toggleFormat()}>
+        <MenuItem icon={<BsGridFill />} onClick={handleSubmit}>
           Categories
         </MenuItem>
-        <MenuItem icon={<AiFillTags />} onClick={() => toggleFormat()}>
+        <MenuItem icon={<AiFillTags />} onClick={handleSubmit}>
           Tags
         </MenuItem>
       </MenuList>
