1c2388980ad67db3282a1a8cbbfd599662fd569b
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-28 10:40:41 +0530

    Cleaned Account Settings Page

--- pages/settings/account.tsx
+++ pages/settings/account.tsx
@@ -1,29 +1,16 @@
 import { GridItem } from "@chakra-ui/react";
 import { NextPage } from "next";
 import React from "react";
-import { QueryClient, useQuery } from "react-query";
-import { dehydrate } from "react-query/hydration";
-import getUserData from "../../apis/getUserData";
 import BoxHeader from "../../components/common/BoxHeader";
 import Footer from "../../components/layouts/Footer";
 import Header from "../../components/layouts/Header";
-import { userPlaceholder } from "../../data/placeholderData";
 import ChangePassword from "../../modules/Settings/Account/ChangePassword";
 import SettingsLayoutGrid from "../../modules/Settings/LayoutGrid";
 import SettingsSidebar from "../../modules/Settings/SettingsSidebar";
-import { UserObject } from "../../modules/User/types";
-import InitUserStore from "../../store/InitUserStore";
 
 const SecuritySettings: NextPage = () => {
-  const { data, status } = useQuery<UserObject, Error>(
-    "getUserData",
-    getUserData,
-    { placeholderData: userPlaceholder }
-  );
-
   return (
     <>
-      <InitUserStore data={data} status={status} />
       <Header />
       <SettingsLayoutGrid>
         <GridItem rowSpan={1} colSpan={4}>
@@ -43,15 +30,3 @@ const SecuritySettings: NextPage = () => {
 };
 
 export default SecuritySettings;
-
-export async function getStaticProps() {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery("getUserData", getUserData);
-
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-}
