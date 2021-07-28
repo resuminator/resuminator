ebd062662bd9111dd0079de5a2309ca4fe711ae1
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-28 10:38:32 +0530

    Removed Redundancies in Settings page

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,37 +1,17 @@
 import { GridItem } from "@chakra-ui/react";
-import { GetServerSidePropsContext, NextPage } from "next";
-import nookies from "nookies";
-import React, { useEffect } from "react";
-import { QueryClient, useQuery } from "react-query";
-import { dehydrate } from "react-query/hydration";
-import getUserData from "../../apis/getUserData";
+import { NextPage } from "next";
+import React from "react";
 import BoxHeader from "../../components/common/BoxHeader";
 import Footer from "../../components/layouts/Footer";
 import Header from "../../components/layouts/Header";
-import { userPlaceholder } from "../../data/placeholderData";
 import PersonalDetails from "../../modules/Settings/General/PersonalDetails";
 import ProfilePhoto from "../../modules/Settings/General/ProfilePhoto";
 import SettingsLayoutGrid from "../../modules/Settings/LayoutGrid";
 import SettingsSidebar from "../../modules/Settings/SettingsSidebar";
-import { UserObject } from "../../modules/User/types";
-import InitUserStore from "../../store/InitUserStore";
 
-export interface SettingsPageProps {
-  token: string;
-}
-
-const Settings: NextPage<SettingsPageProps> = ({ token }) => {
-  const { data, status } = useQuery<UserObject, Error>(
-    "getUserData",
-    () => getUserData(token),
-    { placeholderData: userPlaceholder }
-  );
-
-  useEffect(() => console.log(data), [data]);
-
+const Settings: NextPage = () => {
   return (
     <>
-      <InitUserStore data={data} status={status} />
       <Header />
       <SettingsLayoutGrid>
         <GridItem rowSpan={1} colSpan={4}>
@@ -54,30 +34,3 @@ const Settings: NextPage<SettingsPageProps> = ({ token }) => {
 };
 
 export default Settings;
-
-export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  //Try to get token from cookies.
-  const cookies = nookies.get(ctx);
-  const token = cookies.token;
-
-  //If the token does not exist or is cleared then redirect to login page.
-  if (!token) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/login",
-      },
-    };
-  }
-
-  //If token is present, pass it to the query to fetch data from API
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery("getUserData", () => getUserData(token));
-
-  return {
-    props: {
-      token,
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-};
