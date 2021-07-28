e67fbaff25beaeb18d06599b580ea6ad2b519f97
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-07-28 10:03:14 +0530

    Patching Color in Template and Meta

--- apis/patchTemplate.tsx
+++ apis/patchTemplate.tsx
@@ -32,5 +32,28 @@ const patchTemplate =
 export const patchInput = patchTemplate<ResumeInputsArray>("layout");
 export const patchLayout = patchTemplate<ResumeLayoutObject>("layout");
 export const patchFont = patchTemplate<FontProfile>("font");
-export const patchColor = patchTemplate<ColorProfiles>("color");
 export const patchSpacing = patchTemplate<number>("spacing");
+
+/**
+ * Patches color in Resume Template as well as in Metadata
+ * @param token Auth Token
+ * @param resumeId Resume ID to change the properties
+ * @param body Key value pair to be changed
+ * @returns Response Data: Resume Object
+ */
+export const patchColor = async (
+  token: string = null,
+  resumeId: string,
+  body: { [key: string]: ColorProfiles }
+) => {
+  const [res] = await axios.all([
+    await axios.patch(`${baseUrl}/resume/template/color/${resumeId}`, body, {
+      headers: getHeaders(token),
+    }),
+    await axios.patch(`${baseUrl}/meta/${resumeId}`, body, {
+      headers: getHeaders(token),
+    }),
+  ]);
+
+  return res.data;
+};
