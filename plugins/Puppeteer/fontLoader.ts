0b413230f0f8bb650922cb7bfda9746623392e53
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-16 13:48:58 +0530

    Added Inter Bold and Semibold

--- plugins/Puppeteer/fontLoader.ts
+++ plugins/Puppeteer/fontLoader.ts
@@ -10,12 +10,17 @@ const Fonts = {
       "fontsource/fontsource/2f68479623de914af42e7a42e1064ff668e208fc/packages/inter/files/inter-all-400-normal.woff",
     medium:
       "fontsource/fontsource/2f68479623de914af42e7a42e1064ff668e208fc/packages/inter/files/inter-all-500-normal.woff",
+    semibold:
+      "fontsource/fontsource/2f68479623de914af42e7a42e1064ff668e208fc/packages/inter/files/inter-all-600-normal.woff",
+    bold: "fontsource/fontsource/2f68479623de914af42e7a42e1064ff668e208fc/packages/inter/files/inter-all-700-normal.woff",
   },
 };
 
 const loadFonts = async (instance: typeof chromium) => {
   await instance.font(`${CDNBase}/${Fonts.inter.regular}`);
   await instance.font(`${CDNBase}/${Fonts.inter.medium}`);
+  await instance.font(`${CDNBase}/${Fonts.inter.semibold}`);
+  await instance.font(`${CDNBase}/${Fonts.inter.bold}`);
 };
 
 export default loadFonts;
