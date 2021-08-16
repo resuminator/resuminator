d0112f8e9e04cd38f05b7dc3e40caf032861a744
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-16 12:57:53 +0530

    Added Fontsource As CDN

--- plugins/Puppeteer/fontLoader.ts
+++ plugins/Puppeteer/fontLoader.ts
@@ -1,11 +1,21 @@
 import chromium from "chrome-aws-lambda";
 
-const CDNBase = process.env.NODE_ENV === 'production' ? 'https://rawcdn.githack.com' : 'https://raw.githack.com';
-const FontLocation = 'viveknigam3003/resuminator-next/main/public/fonts';
+const CDNBase =
+  process.env.NODE_ENV === "production"
+    ? "https://rawcdn.githack.com"
+    : "https://raw.githack.com";
+const Fonts = {
+  inter: {
+    regular:
+      "fontsource/fontsource/2f68479623de914af42e7a42e1064ff668e208fc/packages/inter/files/inter-all-400-normal.woff",
+    medium:
+      "fontsource/fontsource/2f68479623de914af42e7a42e1064ff668e208fc/packages/inter/files/inter-all-500-normal.woff",
+  },
+};
 
 const loadFonts = async (instance: typeof chromium) => {
-  await instance.font(`${CDNBase}/${FontLocation}/Inter-Regular.woff2`);
-  await instance.font(`${CDNBase}/${FontLocation}/Inter-Medium.woff2`);
+  await instance.font(`${CDNBase}/${Fonts.inter.regular}`);
+  await instance.font(`${CDNBase}/${Fonts.inter.medium}`);
 };
 
 export default loadFonts;
