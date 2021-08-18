f3ba423b32b2f863a3ef8eba3a1d710fb8b9cc1c
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-18 14:34:46 +0530

    Added Markers in Slider

--- modules/Design/Spacing.tsx
+++ modules/Design/Spacing.tsx
@@ -1,10 +1,10 @@
+import { SliderMark } from "@chakra-ui/react";
 import {
   Slider,
   SliderFilledTrack,
   SliderThumb,
-  SliderTrack,
+  SliderTrack
 } from "@chakra-ui/slider";
-import { Tooltip } from "@chakra-ui/tooltip";
 import React from "react";
 import { patchSpacing } from "../../apis/patchTemplate";
 import Section from "../../components/layouts/Section";
@@ -13,46 +13,36 @@ import { usePatchParams } from "../../hooks/usePatchParams";
 import useResumeStore from "../../store/resume.store";
 import { Result } from "../../store/types";
 
-interface Props {}
+const Marker = ({ value, index }) => {
+  const display = ["Compact", "Normal", "Airy", "Large"];
+  return (
+    <SliderMark fontSize="xs" color="gray" value={value} top="1.5rem">
+      {display[index]}
+    </SliderMark>
+  );
+};
 
-const Spacing = (props: Props) => {
+const Spacing = () => {
   const spacing = useResumeStore((state) => state.spacing);
   const setSpacing = useResumeStore((state) => state.setSpacing);
   const { token, resumeId } = usePatchParams();
   const { createToast } = useCustomToast();
 
-  const getTooltipLabel = (value: number) => {
-    switch (value) {
-      case 0.5:
-        return "Compact";
-      case 1:
-        return "Normal";
-      case 1.5:
-        return "Airy";
-      case 2:
-        return "Large";
-    }
-  };
-
-  const handleSubmit = (value: number) => {
+  const handleSubmit = async (value: number) => {
     setSpacing(value);
 
-    const res = async () =>
-      await patchSpacing(token, resumeId, { spacing: value })
-        .then((res: Result) => {
-          setSpacing(res.template.spacing);
-          return createToast("Resume spacing updated", "success");
-        })
-        .catch(() =>
-          createToast(
-            "Couldn't update resume spacing",
-            "error",
-            "Please try again in sometime"
-          )
-        );
-
-    const timeout = setTimeout(res, 3000);
-    return () => clearTimeout(timeout);
+    return await patchSpacing(token, resumeId, { spacing: value })
+      .then((res: Result) => {
+        setSpacing(res.template.spacing);
+        return createToast("Resume spacing updated", "success");
+      })
+      .catch(() =>
+        createToast(
+          "Couldn't update resume spacing",
+          "error",
+          "Please try again in sometime"
+        )
+      );
   };
 
   return (
@@ -68,7 +58,7 @@ const Spacing = (props: Props) => {
         colorScheme="cyan"
         value={spacing}
         onChange={(value) => handleSubmit(value)}
-        w="60%"
+        w="80%"
         min={0.5}
         max={2}
         step={0.5}
@@ -76,9 +66,10 @@ const Spacing = (props: Props) => {
         <SliderTrack>
           <SliderFilledTrack />
         </SliderTrack>
-        <Tooltip hasArrow label={getTooltipLabel(spacing)}>
-          <SliderThumb bg="cyan.500" boxSize={6} />
-        </Tooltip>
+        {[0.45, 0.91, 1.45, 1.86].map((markValue, index) => (
+          <Marker key={markValue} value={markValue} index={index} />
+        ))}
+        <SliderThumb bg="cyan.500" boxSize={6} mb="2" />
       </Slider>
     </Section>
   );
