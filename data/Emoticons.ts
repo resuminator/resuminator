5b8c16983a340909ae26b3ac480dde06f07d4d5d
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-18 13:16:00 +0530

    Added Phone Icon for App Devs

--- data/Emoticons.ts
+++ data/Emoticons.ts
@@ -1,8 +1,3 @@
-export default [
-  "📄",
-  "💻",
-  "🎨",
-  "🎒",
-  "💼",
-  "⚡",
-];
+//Never remove "📄" emoji -> that is default.
+//Also this array should never be empty
+export default ["📄", "💻", "🎨", "🎒", "💼", "⚡", "📱"];
