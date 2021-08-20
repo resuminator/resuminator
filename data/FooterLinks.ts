d18e9fa74bf8f7da0b00c5be3478c05bbcfb7420
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-20 11:19:25 +0530

    Hidden: Footer Links

--- data/FooterLinks.ts
+++ data/FooterLinks.ts
@@ -4,22 +4,22 @@ interface FooterLinkObject {
 }
 
 const FooterLinks: Array<FooterLinkObject> = [
-  {
-    label: "Sponsor",
-    link: "",
-  },
-  {
-    label: "Broadmap",
-    link: "http://broadmap.resuminator.in/",
-  },
-  {
-    label: "Guides",
-    link: "",
-  },
-  {
-    label: "Docs",
-    link: "",
-  },
+  // {
+  //   label: "Sponsor",
+  //   link: "",
+  // },
+  // {
+  //   label: "Broadmap",
+  //   link: "http://broadmap.resuminator.in/",
+  // },
+  // {
+  //   label: "Guides",
+  //   link: "",
+  // },
+  // {
+  //   label: "Docs",
+  //   link: "",
+  // },
 ];
 
 export default FooterLinks
