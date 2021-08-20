f6147cb6bffd1c6374de5588e5a2c3931ea69ebc
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-20 13:33:49 +0530

    Added Header Button

--- components/website/Header.tsx
+++ components/website/Header.tsx
@@ -1,19 +1,37 @@
-import { Box, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  IconButton, useColorMode, useColorModeValue
+} from "@chakra-ui/react";
 import React from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
 import { LogoWithText } from "../layouts/Logos";
 
 const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Box
       px={{ base: "1rem", md: "4rem", lg: "7rem" }}
+      py="4"
       display="flex"
       alignItems="center"
       justifyContent="space-between"
     >
       <LogoWithText mb="0" letterSpacing="tighter" />
-      <Button colorScheme="purple" size="sm">
-        Get Started
-      </Button>
+      <HStack>
+        <IconButton
+          aria-label="Color-Toggle"
+          onClick={toggleColorMode}
+          icon={colorMode === "light" ? <FaSun /> : <FaMoon />}
+          variant="ghost"
+          colorScheme={useColorModeValue("gray","blue")}
+        />
+        <Button colorScheme="blue" size="sm">
+          Get Started
+        </Button>
+      </HStack>
     </Box>
   );
 };
