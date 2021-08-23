443bbc36ac2abfec9ca5c1a5848fe7643d468131
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-23 16:13:48 +0530

    Sponsor Section Responsive

--- website/Sponsor/index.tsx
+++ website/Sponsor/index.tsx
@@ -1,42 +1,63 @@
-import { Box, Button, ButtonGroup, Icon, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Icon,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import React from "react";
 import { FaHeart, FaPizzaSlice } from "react-icons/fa";
 import { BUY_ME_A_COFFEE } from "../../data/RefLinks";
 import HeadingBox from "../common/HeadingBox";
+import SectionLayout from "../common/SectionLayout";
 
 const Sponsor = () => {
   return (
-    <Box px="8" py="16">
+    <SectionLayout py="16" minH="100vh">
       <HeadingBox
         title="Sponsor this project"
-        subtitle="Your support will help this project impact the lives of thousands of job seekers"
-        subtitleProps={{ fontSize: ["xl"] }}
+        subtitle="Your support will help this project grow and impact the lives of thousands of job seekers"
+        subtitleProps={{
+          fontSize: { base: "lg", sm: "xl", md: "2xl", lg: "3xl" },
+        }}
       >
-        <Icon as={FaHeart} color="red" fontSize="3xl" mx="2" />
+        <Icon
+          as={FaHeart}
+          color="red"
+          fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}
+          mx="2"
+        />
       </HeadingBox>
 
-      <Text mb="8">
-        Resuminator is a free and open source project built and maintained by a
-        duo from India, and is backed by our community and contributors. You can
-        support the development of this project if you believe this project
-        added value to your life or could potentially add value to the lives of
-        others.
-      </Text>
-      <ButtonGroup>
-        {/* <Button leftIcon={<FaHeart />} colorScheme="purple">
+      <Box px={["0", "4", "16", "24", "72"]} pt="4" pb="8">
+        <Text mb="8" fontSize={{ base: "lg", md: "xl", lg: "2xl" }}>
+          Resuminator is a free and open source project built and maintained by
+          a duo from India, and is backed by our community and contributors. You
+          can support the development of this project if you believe this
+          project added value to your life or could potentially add value to the
+          lives of others.
+        </Text>
+        <Stack
+          justifyContent="center"
+          isInline={useBreakpointValue({ md: true })}
+        >
+          {/* <Button leftIcon={<FaHeart />} colorScheme="purple">
           Become a supporter
         </Button> */}
-        <Button
-          as="a"
-          href={BUY_ME_A_COFFEE}
-          target="_blank"
-          leftIcon={<FaPizzaSlice />}
-          colorScheme="purple"
-        >
-          Buy us a pizza
-        </Button>
-      </ButtonGroup>
-    </Box>
+          <Button
+            as="a"
+            href={BUY_ME_A_COFFEE}
+            target="_blank"
+            leftIcon={<FaPizzaSlice />}
+            colorScheme="yellow"
+            size={useBreakpointValue({ md: "lg" })}
+          >
+            Buy us a pizza
+          </Button>
+        </Stack>
+      </Box>
+    </SectionLayout>
   );
 };
 
