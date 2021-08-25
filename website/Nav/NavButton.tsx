8cc87b3d0139efcf53e5ea47adbb4ee5665015ff
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-25 22:33:49 +0530

    Fix: Removed Collapsible Menu

--- website/Nav/NavButton.tsx
+++ website/Nav/NavButton.tsx
@@ -1,15 +1,11 @@
 import {
-    Box,
-    Button,
-    Collapse,
-    IconButton,
-    Stack,
-    useColorModeValue,
-    useDisclosure
+  Box,
+  IconButton,
+  useColorModeValue,
+  useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
 import { FiMenu } from "react-icons/fi";
-import { navLinkDetails } from "./links";
 
 const NavButton = () => {
   const { isOpen, onToggle } = useDisclosure();
@@ -23,25 +19,6 @@ const NavButton = () => {
         colorScheme={useColorModeValue("gray", "blue")}
         onClick={onToggle}
       />
-      <Collapse in={isOpen} animateOpacity>
-        <Stack
-          minH="60vh"
-          background={useColorModeValue("whiteAlpha.800", "gray.800")}
-        >
-          {navLinkDetails.map((item) => (
-            <Button
-              key={item.label}
-              as="a"
-              href={item.link}
-              variant="ghost"
-              size="sm"
-              fontWeight="normal"
-            >
-              {item.label}
-            </Button>
-          ))}
-        </Stack>
-      </Collapse>
     </Box>
   );
 };
