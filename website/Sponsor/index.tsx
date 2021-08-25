8f3238e6fd0a3a13fb486ad3c682d950c47ff690
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-25 19:33:43 +0530

    Analytics: Sponsor CTA

--- website/Sponsor/index.tsx
+++ website/Sponsor/index.tsx
@@ -2,10 +2,15 @@ import { Box, Button, Stack, Text, useBreakpointValue } from "@chakra-ui/react";
 import React from "react";
 import { FaPizzaSlice } from "react-icons/fa";
 import { BUY_ME_A_COFFEE } from "../../data/RefLinks";
+import mp from "../../services/mixpanel";
 import HeadingBox from "../common/HeadingBox";
 import SectionLayout from "../common/SectionLayout";
 
 const Sponsor = () => {
+  const trackMetric = (from: string, to: string) => {
+    mp.track("Sponsor CTA Trigger", { from, to });
+  };
+
   return (
     <SectionLayout minH="100vh" aria-label="Sponsor Project">
       <HeadingBox
@@ -24,31 +29,31 @@ const Sponsor = () => {
           project added value to your life or could potentially add value to the
           lives of others.
         </Text>
-        
       </Box>
       <Stack
-          justifyContent="center"
-          isInline={useBreakpointValue({ md: true })}
-        >
-          {/* <Button
+        justifyContent="center"
+        isInline={useBreakpointValue({ md: true })}
+      >
+        {/* <Button
               leftIcon={<FaHeart />}
               colorScheme="purple"
               size={useBreakpointValue({ md: "lg" })}
             >
               Become a supporter
             </Button> */}
-          <Button
-            as="a"
-            href={BUY_ME_A_COFFEE}
-            target="_blank"
-            leftIcon={<FaPizzaSlice />}
-            colorScheme="yellow"
-            size={useBreakpointValue({ md: "lg" })}
-            color="#1A202C"
-          >
-            Buy us a pizza
-          </Button>
-        </Stack>
+        <Button
+          as="a"
+          href={BUY_ME_A_COFFEE}
+          target="_blank"
+          leftIcon={<FaPizzaSlice />}
+          colorScheme="yellow"
+          size={useBreakpointValue({ md: "lg" })}
+          color="#1A202C"
+          onClick={() => trackMetric("Buy Me a Coffee", BUY_ME_A_COFFEE)}
+        >
+          Buy us a pizza
+        </Button>
+      </Stack>
     </SectionLayout>
   );
 };
