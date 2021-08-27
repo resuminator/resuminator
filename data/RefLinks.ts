e594429a26e5eabc8ab23edb0aab5e1e43eb3e44
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-27 11:34:51 +0530

    Added Twitter

--- data/RefLinks.ts
+++ data/RefLinks.ts
@@ -15,3 +15,5 @@ export const BROADMAP_HOMEPAGE = "https://broadmap.resuminator.in/";
 export const BROADMAP_TWITTER = "https://twitter.com/broadmaps";
 export const BROADMAP_SHARE_TWITTER =
   "https://www.twitter.com/share?url=http%3A%2F%2Fbroadmap.resuminator.in%2F&via=revue&text=I%20just%20discovered%20%40broadmaps%20by%20%40resuminator.%20The%20concept%20of%20getting%20a%20%22playlist%22%20of%20resources%20every%20week%20seems%20exciting%20to%20me%21%20Calling%20all%20curious%20folks%20to%20give%20it%20a%20try%20for%20sure.&related=revue";
+export const RESUMINATOR_SHARE_TWITTER =
+  "https://www.twitter.com/share?url=https%3A%2F%2Fresuminator.in%2F&text=I%20just%20signed%20up%20for%20%40resuminator%20and%20I%27m%20excited%20to%20try%20it%20out%21%20Calling%20out%20all%20job%20seekers%20to%20give%20it%20a%20try%20this%20season.%20%23ResuminatorResume%20";
