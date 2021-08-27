37f47fba30119bf0ec30c74c4a5d882b59c25f2d
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-27 12:47:14 +0530

    Analytics: External Link Trigger on Graph

--- website/Graph/index.tsx
+++ website/Graph/index.tsx
@@ -1,16 +1,26 @@
 import { AspectRatio, Image, Text, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 import TextLink from "../../components/common/TextLink";
+import mp from "../../services/mixpanel";
 import HeadingBox from "../common/HeadingBox";
 import SectionLayout from "../common/SectionLayout";
 
 const Graph = () => {
+  const trackMetric = (from: string, to: string) => {
+    mp.track("External Link Trigger", { from, to });
+  };
+
+  const author = {
+    name: "Apoorva Agrawal",
+    website: "https://pixelkanya.com",
+  };
+
   return (
     <SectionLayout>
       <HeadingBox
         title="Better than your doc resume, faster than a LaTeX editor"
         subtitle="Landing a sweet spot between customization and ease ⚡"
-        titleProps={{ color: useColorModeValue("purple.600", "purple.400") }}
+        titleProps={{ color: useColorModeValue("purple.500", "purple.400") }}
         py={{ base: "0" }}
         pt={{ base: "8", sm: "10", md: "16", lg: "20" }}
         subtitleProps={{
@@ -24,14 +34,15 @@ const Graph = () => {
       <Text
         fontSize="xs"
         color="gray"
-        opacity="80%"
+        opacity={useColorModeValue("100%", "80%")}
         textAlign={{ sm: "center" }}
       >
         Illustration by{" "}
         <TextLink
-          text="Apoorva Agrawal"
-          link="https://pixelkanya.com"
-          color="purple.500"
+          text={author.name}
+          link={author.website}
+          color={useColorModeValue("purple.500", "purple.400")}
+          onClick={() => trackMetric("Graph Attribution", author.website)}
         />
       </Text>
     </SectionLayout>
