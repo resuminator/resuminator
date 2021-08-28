63e785ea127978c9e85ed66bf4ab9dba160e9936
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-28 11:41:47 +0530

    Tracing: Removed Sentry

--- next.config.js
+++ next.config.js
@@ -1,23 +1,9 @@
 /* eslint-disable no-undef */
-const { withSentryConfig } = require("@sentry/nextjs");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const SentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
+const nextConfig = {}
 
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
 
-const nextConfig = {};
-
-module.exports = withBundleAnalyzer(
-  withSentryConfig(nextConfig, SentryWebpackPluginOptions)
-);
+module.exports = withBundleAnalyzer(nextConfig);
