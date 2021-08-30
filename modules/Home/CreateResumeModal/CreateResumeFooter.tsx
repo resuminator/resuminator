a6f6b689371d716750d6b316a993aa5147e87818
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-30 22:23:40 +0530

    Added License

--- modules/Home/CreateResumeModal/CreateResumeFooter.tsx
+++ modules/Home/CreateResumeModal/CreateResumeFooter.tsx
@@ -1,6 +1,26 @@
+/*
+    Resuminator, Web App and the Website for Resuminator
+    Copyright (C) 2021 Resuminator Authors
+
+    This file is part of Resuminator.
+
+    Resuminator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Resuminator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Resuminator.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { Button, HStack, ModalFooter } from "@chakra-ui/react";
 import React from "react";
-import { FiArrowRight, FiExternalLink } from "react-icons/fi";
+import { FiArrowRight } from "react-icons/fi";
 import { Status } from "../../../utils/constants";
 import { Method } from "../CreateResumeModal";
 
@@ -11,7 +31,12 @@ interface Props {
   status?: Status;
 }
 
-const CreateResumeFooter: React.FC<Props> = ({ method, onCloseCallback, actionCallback, status }) => {
+const CreateResumeFooter: React.FC<Props> = ({
+  method,
+  onCloseCallback,
+  actionCallback,
+  status,
+}) => {
   return (
     <ModalFooter display="flex" justifyContent="flex-end">
       {/* Available when template gallery implemented */}
@@ -29,7 +54,7 @@ const CreateResumeFooter: React.FC<Props> = ({ method, onCloseCallback, actionCa
           colorScheme="purple"
           rightIcon={<FiArrowRight />}
           isDisabled={method === null}
-          isLoading={status===Status.loading}
+          isLoading={status === Status.loading}
           loadingText="Creating new resume"
           onClick={() => actionCallback(method)}
         >
