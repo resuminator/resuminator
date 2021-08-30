4b14f876fc78d207c9236b08528712781ca10ec5
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-30 22:41:47 +0530

    Added License

--- website/Hero/PrimaryCTA.tsx
+++ website/Hero/PrimaryCTA.tsx
@@ -1,3 +1,23 @@
+/*
+    Resuminator, Web App and the Website for Resuminator
+    Copyright (C) 2021 Resuminator Authors
+
+    This file is part of Resuminator.
+
+    Resuminator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Resuminator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Resuminator.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { Button, useBreakpointValue } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React from "react";
