ae8a9c8e8ae2b928e2846b90ee665d23a6b377ac
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-30 22:28:15 +0530

    Added License

--- modules/SEO/ogImages.tsx
+++ modules/SEO/ogImages.tsx
@@ -1,3 +1,23 @@
+/*
+    Resuminator, Web App and the Website for Resuminator
+    Copyright (C) 2021 Resuminator Authors
+
+    This file is part of Resuminator.
+
+    Resuminator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Resuminator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Resuminator.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 export const OGImage =
   "https://user-images.githubusercontent.com/30192068/130635620-7d41f775-151a-4f00-9899-e891224870dd.png";
 export const OGLogo =
