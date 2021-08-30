7f157ba61e29cde18069ebaaac9e1b3a4e362294
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-08-30 22:00:29 +0530

    License: Added to APIs

--- apis/meta.tsx
+++ apis/meta.tsx
@@ -1,3 +1,23 @@
+/*
+    Resuminator, Web App and the Website for Resuminator
+    Copyright (C) 2021 Resuminator Authors
+
+    This file is part of Resuminator.
+
+    Resuminator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Resuminator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Resuminator.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import axios from "axios";
 import { baseUrl } from "../config/apis";
 import getHeaders from "../utils/headers";
@@ -15,7 +35,7 @@ export const getUserData = async (token: string = null) => {
 };
 
 /**
- * 
+ *
  * @param token Firebase ID Token
  * @param _id Resume ID
  * @param body Key:Value pair to update example `{profileName: "XYZ"}` or `{color: "blue"}`
