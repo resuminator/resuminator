7219f493cd60e70f1fde7dfb843bbb09ade63e62
Vivek Nigam <viveknigam.nigam3@gmail.com>
2021-09-23 09:39:00 +0000

    Patch: Added Timer to Page

--- website/Hacktoberfest/HfHero.tsx
+++ website/Hacktoberfest/HfHero.tsx
@@ -1,12 +1,22 @@
+import { useColorModeValue } from "@chakra-ui/color-mode";
 import { Image } from "@chakra-ui/image";
 import { Center, Stack, Text } from "@chakra-ui/layout";
 import { useBreakpointValue } from "@chakra-ui/media-query";
+import dynamic from "next/dynamic";
 import React from "react";
 import { LogoWithText } from "../../components/layouts/Logos";
+import { HFColors } from "./hf_colors";
+const CountdownTimer = dynamic(() => import("./CountdownTimer"), {
+  ssr: false,
+});
 
 interface Props {}
 
 const HfHero = (props: Props) => {
+  const colorModeDefaultLogo = useColorModeValue(
+    "/logos/text_dark.svg",
+    "/logos/text_light.svg"
+  );
   return (
     <Center minHeight="100vh" flexDir="column">
       <Stack
@@ -16,9 +26,9 @@ const HfHero = (props: Props) => {
         <Image
           src="/web/hf_logo_light.svg"
           boxSize={{ base: "16rem", md: "18rem", xl: "20rem" }}
-          height="8rem"
         />
         <Text
+          display={{ base: "none", md: "initial" }}
           fontWeight="bold"
           fontSize={{ base: "4xl", xl: "5xl" }}
           position="relative"
@@ -28,14 +38,20 @@ const HfHero = (props: Props) => {
         >
           +
         </Text>
-        <LogoWithText
+        <Image
+          src={colorModeDefaultLogo}
           width={{ base: "160px", md: "240px", xl: "400px" }}
           height={{ base: "32px", md: "48px", xl: "80px" }}
         />
       </Stack>
-      <Text fontWeight="semibold" fontSize={{ base: "xl", xl: "2xl" }}>
+      <Text
+        fontWeight="semibold"
+        fontSize={{ base: "lg", md: "xl", lg: "xl", xl: "2xl" }}
+        color={useColorModeValue(HFColors.text.secondary, HFColors.text.light)}
+      >
         1st October 2021 to 31st October 2021
       </Text>
+      <CountdownTimer />
     </Center>
   );
 };
---
+++ website/Hacktoberfest/hf_colors.ts
@@ -0,0 +1,10 @@
+export const HFColors = {
+  bg: {
+    primary: "#FFEDD5",
+  },
+  text: {
+    primary: "#F74700",
+    secondary: "#2B3531",
+    light: "#F0FDF4",
+  },
+};
