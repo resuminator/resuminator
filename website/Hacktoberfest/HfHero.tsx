a8cf2f58b22495479cf8215179503b2709de127e
Vivek Nigam <viveknigam.nigam3@gmail.com>
2021-09-25 10:55:42 +0000

    Patch: Added License Header

--- website/Hacktoberfest/HfHero.tsx
+++ website/Hacktoberfest/HfHero.tsx
@@ -1,10 +1,29 @@
+/*
+    Resuminator, Web App and the Website for Resuminator
+    Copyright (C) 2021 Resuminator Authors
+
+    This file is part of Resuminator.
+
+    Resuminator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Resuminator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Resuminator.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { useColorModeValue } from "@chakra-ui/color-mode";
 import { Image } from "@chakra-ui/image";
 import { Center, Stack, Text } from "@chakra-ui/layout";
 import { useBreakpointValue } from "@chakra-ui/media-query";
 import dynamic from "next/dynamic";
 import React from "react";
-import { LogoWithText } from "../../components/layouts/Logos";
 import { HFColors } from "./hf_colors";
 const CountdownTimer = dynamic(() => import("./CountdownTimer"), {
   ssr: false,
