c0680d06005ea0c3aa9da177f899b91f7cdc1928
Vivek Nigam <viveknigam.nigam3@gmail.com>
2021-09-25 12:59:42 +0000

    Updated Assets

--- pages/hacktoberfest.tsx
+++ pages/hacktoberfest.tsx
@@ -8,6 +8,12 @@ import Nav from "../website/Nav";
 
 interface Props {}
 
+const HF_BG = {
+  light:
+    "https://user-images.githubusercontent.com/30192068/134772360-e45e99e9-f7f5-483c-bdb0-474de4b919df.png",
+  dark: "https://user-images.githubusercontent.com/30192068/134772359-22ecf056-5859-4ac3-9f9f-b26ae4a86bb0.png",
+};
+
 const Hacktoberfest = (props: Props) => {
   return (
     <>
@@ -17,8 +23,8 @@ const Hacktoberfest = (props: Props) => {
         width="100%"
         minH="100vh"
         backgroundImage={useColorModeValue(
-          "url(/web/hf_light.png)",
-          "url(/web/hf_dark.png)"
+          `url(${HF_BG.light})`,
+          `url(${HF_BG.dark})`
         )}
         backgroundRepeat="no-repeat"
         backgroundPosition="center"
