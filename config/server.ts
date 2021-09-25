c1001a1967a370d46684460bb81c45a2cc45b4eb
Vivek Nigam <viveknigam.nigam3@gmail.com>
2021-09-25 13:39:18 +0000

    Updated Server URL

--- config/server.ts
+++ config/server.ts
@@ -18,10 +18,7 @@
     along with Resuminator.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-const API_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_API_BASE_URL
-    : "https://r8staging.herokuapp.com";
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
 export const API_VERSION = "v0.2.0"
 
