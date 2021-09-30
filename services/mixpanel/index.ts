daa7271d8eb285bf1d95b14d267d71ae1dd8c9d2
Vivek Nigam <viveknigam.nigam3@gmail.com>
2021-09-30 04:34:35 +0000

    Patch: Added Reason

--- services/mixpanel/index.ts
+++ services/mixpanel/index.ts
@@ -21,7 +21,8 @@
 import mixpanel from "mixpanel-browser";
 
 //If there is no token in the env variables, the TEST_TOKEN will initialize Mixpanel
-//It does not throw error just that nothing will be logged.
+//It does not throw error just that nothing will be logged. 
+//This is to make Mixpanel as optional dependency
 const KEY = process.env.NEXT_PUBLIC_MIXPANEL || 'TEST_TOKEN';
 
 mixpanel.init(KEY);
