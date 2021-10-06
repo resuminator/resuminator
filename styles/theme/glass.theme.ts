49bc8a319f58f0ff0428269e8f0af769c0e708c0
Vivek Nigam <viveknigam.nigam3@gmail.com>
2021-10-06 04:40:13 +0000

    chore: formatting styles

--- styles/theme/glass.theme.ts
+++ styles/theme/glass.theme.ts
@@ -22,9 +22,9 @@ import { mode } from "@chakra-ui/theme-tools";
 
 const Frost = (props) => ({
   ".frost": {
-    "backdropFilter": "blur(3px)",
-    background: mode("rgba(255, 255, 255, 0.04)", "rgba(0, 0, 0, 0.04)")(props),
-  },
+    backdropFilter: "blur(3px)",
+    background: mode("rgba(255, 255, 255, 0.04)", "rgba(0, 0, 0, 0.04)")(props)
+  }
 });
 
 export default Frost;
--- styles/theme/scrollbar.theme.ts
+++ styles/theme/scrollbar.theme.ts
@@ -22,25 +22,25 @@ import { mode } from "@chakra-ui/theme-tools";
 
 const ScrollBarTheme = (props) => ({
   ".resume-inputs::-webkit-scrollbar": {
-    display: "none",
+    display: "none"
   },
   ".viewer::-webkit-scrollbar": {
-    display: "none",
+    display: "none"
   },
   "::-webkit-scrollbar": {
-    width: "10px",
+    width: "10px"
   },
   "::-webkit-scrollbar-thumb": {
     transition: "0.2s all",
     background: mode("rgba(150,150,150,0.5)", "rgba(255,255,255,0.2)")(props),
-    borderRadius: "10px",
+    borderRadius: "10px"
   },
   "::-webkit-scrollbar-thumb:hover": {
-    background: mode("rgba(150,150,150,0.8)", "rgba(150,150,150,0.5)")(props),
+    background: mode("rgba(150,150,150,0.8)", "rgba(150,150,150,0.5)")(props)
   },
   "::-webkit-scrollbar-track": {
-    background: mode("rgba(150,150,150,0.3)", "rgba(150,150,150,0.1)")(props),
-  },
+    background: mode("rgba(150,150,150,0.3)", "rgba(150,150,150,0.1)")(props)
+  }
 });
 
 export default ScrollBarTheme;
