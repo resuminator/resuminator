69772d96d22715a35bfbe78933dde75c08544c07
Vivek Nigam <viveknigam.nigam3@gmail.com>
2021-10-06 04:44:00 +0000

    chore: formatting data

--- data/Hints/certification.ts
+++ data/Hints/certification.ts
@@ -22,7 +22,10 @@ import { SectionHints } from "./types";
 
 const CertificationHints: SectionHints = {
   title: "How to show certifications",
-  content: [{ type: "p", body: "DOs" }, { type: "p", body: "DON'Ts" }],
+  content: [
+    { type: "p", body: "DOs" },
+    { type: "p", body: "DON'Ts" }
+  ]
 };
 
 export default CertificationHints;
--- data/RefLinks.ts
+++ data/RefLinks.ts
@@ -31,11 +31,12 @@ export const WEBSITE = "https://resuminator.in";
 export const BUY_ME_A_COFFEE = "https://www.buymeacoffee.com/resuminator";
 export const OPEN_COLLECTIVE = "https://opencollective.com/resuminator";
 export const GITHUB_REPO = "https://github.com/resuminator/resuminator";
-export const GITHUB_REPO_GFI = "https://github.com/resuminator/resuminator/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22";
+export const GITHUB_REPO_GFI =
+  "https://github.com/resuminator/resuminator/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22";
 export const BROADMAP_HOMEPAGE = "https://broadmap.resuminator.in/";
 export const BROADMAP_TWITTER = "https://twitter.com/broadmaps";
 export const BROADMAP_SHARE_TWITTER =
   "https://www.twitter.com/share?url=https%3A%2F%2Fbroadmap.xyz&text=I%20just%20discovered%20%40broadmaps%20by%20%40_viveknigam_%20%26%20%40_mercurybuddy.%20It%27s%20like%20a%20high-signal%20information%20diet%20about%20products%2C%20tech%2C%20and%20growth%21%20Calling%20all%20curious%20folks%20to%20give%20it%20a%20try%20for%20sure.";
 export const RESUMINATOR_SHARE_TWITTER =
   "https://www.twitter.com/share?url=https%3A%2F%2Fresuminator.in%2F&text=I%20just%20signed%20up%20for%20%40resuminator%20and%20I%27m%20excited%20to%20try%20it%20out%21%20Calling%20out%20all%20job%20seekers%20to%20give%20it%20a%20try%20this%20season.%20%23ResuminatorResume%20";
-export const CONTRIBUTE_HINTS="";
+export const CONTRIBUTE_HINTS = "";
