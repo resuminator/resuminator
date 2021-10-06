164980d2823a966aa5e6deb838b34c929cf9bd8f
Vivek Nigam <viveknigam.nigam3@gmail.com>
2021-10-06 04:40:13 +0000

    chore: formatting styles

--- styles/theme/index.ts
+++ styles/theme/index.ts
@@ -26,29 +26,29 @@ import Frost from "./glass.theme";
 const DefaultTheme: ChakraTheme = extendTheme({
   fonts: {
     heading: "Manrope",
-    body: "Inter",
+    body: "Inter"
   },
   config: {
     initialColorMode: "light",
-    useSystemColorMode: false,
+    useSystemColorMode: false
   },
   colors: {
     gray: {
-      800: "#1A202C",
+      800: "#1A202C"
     },
     brand: {
       400: "#4cc9f0",
       500: "#4361ee",
-      600: "#3a0ca3",
-    },
+      600: "#3a0ca3"
+    }
   },
   styles: {
     global: (props) => ({
       ...ProseMirror(props),
       ...ScrollBarTheme(props),
-      ...Frost(props),
-    }),
-  },
+      ...Frost(props)
+    })
+  }
 });
 
 export default DefaultTheme;
