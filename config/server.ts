4dc814bd99f7127a88e4e54bea104f6b37d80a1f
Vivek Nigam <viveknigam.nigam3@gmail.com>
2021-10-06 04:44:30 +0000

    chore: formatting config

--- config/server.ts
+++ config/server.ts
@@ -18,8 +18,8 @@
     along with Resuminator.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export const API_VERSION = "v0.2.0"
+export const API_VERSION = "v0.2.0";
 
 export default API_URL;
