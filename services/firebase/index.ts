be2ca32ae5c0fdcfc2f921abbe1b72b0bab88641
Vivek Nigam <viveknigam.nigam3@gmail.com>
2021-10-06 04:41:23 +0000

    chore: formatting services

--- services/firebase/index.ts
+++ services/firebase/index.ts
@@ -30,7 +30,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 if (typeof window !== "undefined" && !firebase.apps.length) {
