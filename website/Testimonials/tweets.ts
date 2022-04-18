175cdc84331979516649d21d0d171599c0456814
Vivek Nigam <viveknigam.nigam3@gmail.com>
2022-04-18 16:39:42 +0000

    fix: updated testimonial order

--- website/Testimonials/tweets.ts
+++ website/Testimonials/tweets.ts
@@ -21,16 +21,16 @@
 import { TweetCardProps } from "./TweetCard";
 
 const tweets: Array<TweetCardProps> = [
-  // {
-  //   name: "Vivek Nigam",
-  //   handle: "@_viveknigam_",
-  //   date: "Aug 12, 2021",
-  //   image:
-  //     "https://pbs.twimg.com/profile_images/1438358493799452672/vNWqLAHP_400x400.jpg",
-  //   content:
-  //     "Just added API docs for @resuminator v2 with the new @github editor feature. Super smooth and quick! Much needed! ⚡",
-  //   url: "https://twitter.com/_viveknigam_/status/1425683094674104322",
-  // },
+  {
+    name: "Manav Kashyap",
+    handle: "@manavkashyap24",
+    date: "Aug 31, 2021",
+    image:
+      "https://pbs.twimg.com/profile_images/1402685737649115136/j4jHnBhh_400x400.jpg",
+    content:
+      "Hardwork is appreciated. I did checkout v1 and now v2 is definitely an improvement. The UI is very impressive. The light and dark modes are smooth as  whiskey. I want you and the team to deliver highest level of sophistication. \n Cheers 🥂",
+    url: "https://twitter.com/manavkashyap24/status/1432668624179720194"
+  },
   {
     name: "ANKUR (He/Him)",
     handle: "@DEV__Ankur",
@@ -41,14 +41,13 @@ const tweets: Array<TweetCardProps> = [
     url: "https://twitter.com/DEV__Ankur/status/1429666028066271233"
   },
   {
-    name: "Manav Kashyap",
-    handle: "@manavkashyap24",
-    date: "Aug 31, 2021",
+    name: "Yash Sehgal",
+    handle: "@yashsehgaldev",
+    date: "Oct 15, 2021",
     image:
-      "https://pbs.twimg.com/profile_images/1402685737649115136/j4jHnBhh_400x400.jpg",
-    content:
-      "Hardwork is appreciated. I did checkout v1 and now v2 is definitely an improvement. The UI is very impressive. The light and dark modes are smooth as  whiskey. I want you and the team to deliver highest level of sophistication. \n Cheers 🥂",
-    url: "https://twitter.com/manavkashyap24/status/1432668624179720194"
+      "https://user-images.githubusercontent.com/30192068/163839556-36eab2b8-5c4f-4c14-9c2c-86775885d242.png",
+    content: `Resuminator is one of those products which can't stop suggesting to everyone to use. Really Really handy to use and have a good sense of design as well. You are doing great work. Love to see the Resuminator team growing :D`,
+    url: ""
   },
   {
     name: "Himanshu (He/Him)",
@@ -67,15 +66,6 @@ const tweets: Array<TweetCardProps> = [
       "https://user-images.githubusercontent.com/30192068/163838992-487042dd-a75c-44dc-a7f9-1075e87f3ab9.png",
     content: `Woah! Thats amazing, Vivek. Have used Resuminator before, it's UI UX works like a charm. `,
     url: "https://twitter.com/sohamsshah_/status/1503648636088971265"
-  },
-  {
-    name: "Yash Sehgal",
-    handle: "@yashsehgaldev",
-    date: "Oct 15, 2021",
-    image:
-      "https://user-images.githubusercontent.com/30192068/163839556-36eab2b8-5c4f-4c14-9c2c-86775885d242.png",
-    content: `Resuminator is one of those products which can't stop suggesting to everyone to use. Really Really handy to use and have a good sense of design as well. You are doing great work. Love to see the Resuminator team growing :D`,
-    url: ""
   },
   {
     name: "Joydeep Singha",
