2460dfdd6e0b7f3e51b105098731f4cf90dca4a3
Vivek Nigam <viveknigam.nigam3@gmail.com>
2022-08-21 17:22:20 +0530

    feat: added signup benefits

---
+++ data/SignupBenefits.ts
@@ -0,0 +1,24 @@
+import { BenefitList } from "../modules/Auth/AuthBenefits";
+
+export const signupBenefits: BenefitList = [
+  {
+    iconSrc: "/icons/LayoutIcon.svg",
+    text: "Toggle your resume layout between one or two column depending on your details or your recruiter's requirements."
+  },
+  {
+    iconSrc: "/icons/DesignIcon.svg",
+    text: "Customize resume color with our Powerful Design Controls"
+  },
+  {
+    iconSrc: "/icons/ContactIcon.svg",
+    text: "Show that you exsist to recruiters by attaching LinkedIn, GitHub, Twitter, Portfolio, Behance and other portfolio links"
+  },
+  {
+    iconSrc: "/icons/MultipleResumeIcon.svg",
+    text: "Create multiple resumes as per your need and customize them"
+  },
+  {
+    iconSrc: "/icons/ProfilePhotoIcon.svg",
+    text: "Add separate profile pictures to your resume"
+  }
+];
