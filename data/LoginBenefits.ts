365ba554e81026ab877b48ea2e0b9a9899bd52d0
Vivek Nigam <viveknigam.nigam3@gmail.com>
2022-08-21 14:09:54 +0530

    feat: added login benefit list

---
+++ data/LoginBenefits.ts
@@ -0,0 +1,20 @@
+import { BenefitList } from "../modules/Auth/AuthBenefits";
+
+export const loginBenefits: BenefitList = [
+  {
+    iconSrc: "/icons/RearrangeIcon.svg",
+    text: "Update and rearrange your resume section with Drag-n-Drop Layouts."
+  },
+  {
+    iconSrc: "/icons/EyeIcon.svg",
+    text: "Keep only relevant data while downloading your resume with Easy Toggles."
+  },
+  {
+    iconSrc: "/icons/AddSectionIcon.svg",
+    text: "Add custom section as per your need for achievements, contributions, hobbies and anything!"
+  },
+  {
+    iconSrc: "/icons/ExpandIcon.svg",
+    text: "Fit more content by alter spacing in a dedicated Design section."
+  }
+];
