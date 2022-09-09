2dcbd42090d9dd5e433a294ad248c54d786530ff
Himanshu Garg <garg_himanshu@outlook.com>
2022-09-09 13:24:38 +0530

    chore: update team image

--- website/About/teamDetails.ts
+++ website/About/teamDetails.ts
@@ -23,7 +23,7 @@ import { TeamMember } from "./types";
 export const teamDetails: Array<TeamMember> = [
   {
     image:
-      "https://user-images.githubusercontent.com/30192068/163840746-0747b937-4dc8-443a-a367-c8df7afa6afd.png",
+      "https://www.gravatar.com/avatar/516fd0624a35f74e54802fea778abf41.jpg?s=1200",
     fullName: "Vivek Nigam",
     jobTitle: "Creator, Lead Developer",
     bio: "Collects dozens of typefaces, uses only two. Part-time mentor. Frontend and Design, execution and strategy.",
