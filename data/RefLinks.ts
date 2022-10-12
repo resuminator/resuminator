df2161e45af1fbdaa0b3feadeda0990153862b23
Vivek Nigam <viveknigam.nigam3@gmail.com>
2022-10-12 11:40:44 +0530

    feat: added contribute hints link

--- data/RefLinks.ts
+++ data/RefLinks.ts
@@ -39,4 +39,4 @@ export const BROADMAP_SHARE_TWITTER =
   "https://www.twitter.com/share?url=https%3A%2F%2Fbroadmap.xyz&text=I%20just%20discovered%20%40broadmaps%20by%20%40_viveknigam_%20%26%20%40_mercurybuddy.%20It%27s%20like%20a%20high-signal%20information%20diet%20about%20products%2C%20tech%2C%20and%20growth%21%20Calling%20all%20curious%20folks%20to%20give%20it%20a%20try%20for%20sure.";
 export const RESUMINATOR_SHARE_TWITTER =
   "https://www.twitter.com/share?url=https%3A%2F%2Fresuminator.in%2F&text=I%20just%20signed%20up%20for%20%40resuminator%20and%20I%27m%20excited%20to%20try%20it%20out%21%20Calling%20out%20all%20job%20seekers%20to%20give%20it%20a%20try%20this%20season.%20%23ResuminatorResume%20";
-export const CONTRIBUTE_HINTS = "";
+export const CONTRIBUTE_HINTS = "https://github.com/resuminator/resuminator/issues/154";
