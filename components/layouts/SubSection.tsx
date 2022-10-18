e2dd9401187f220063f739d0a9a5b681f4de1088
Vivek Nigam <viveknigam.nigam3@gmail.com>
2022-10-18 20:28:47 +0530

    feat: added subsection layout

---
+++ components/layouts/SubSection.tsx
@@ -0,0 +1,26 @@
+import { Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  title?: string;
+  subtitle?: string;
+}
+
+const Subsection: React.FC<Props> = ({ title, subtitle }) => {
+  return (
+    <VStack align={"start"} mb="4">
+      {title && (
+        <Text fontSize={"sm"} fontWeight={"bold"}>
+          {title}
+        </Text>
+      )}
+      {subtitle && (
+        <Text fontSize={"xs"} color="gray.500">
+          {subtitle}
+        </Text>
+      )}
+    </VStack>
+  );
+};
+
+export default Subsection;
