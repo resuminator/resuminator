a1e95165fcf4abb1bd38ea3ccbd2d3e8e554a351
Sayandeep Ghosh <77008026+sayang14@users.noreply.github.com>
2022-10-18 11:12:39 +0530

    Update certification.ts

--- data/Hints/certification.ts
+++ data/Hints/certification.ts
@@ -23,7 +23,7 @@ import { SectionHints } from "./types";
 const CertificationHints: SectionHints = {
   title: "How to show certifications",
   content: [
-    { type: "p", body: "Please add any certifications that you have done which involves completing any accredited institutions course and passing it's requirements" },
+    { type: "p", body: "Adding certifications that you have done involving completing any accredited institution's course and passing it's requirements will help you to gain trust of the recruiters that you have right qualifications for the job" },
     { type: "p", body: "Please don't just describe the course. We recommend you to attach any certification proof(link provided) along with the course details" }
   ]
 };
