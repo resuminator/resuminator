e502444c5754366118b2507665a0e92a7038dc09
Sayandeep Ghosh <77008026+sayang14@users.noreply.github.com>
2022-10-19 12:33:53 +0530

    Update projects.ts

--- data/Hints/projects.ts
+++ data/Hints/projects.ts
@@ -22,7 +22,18 @@ import { SectionHints } from "./types";
 
 const ProjectHints: SectionHints = {
   title: "Hints on adding projects",
-  content: [{ type: "p", body: "" }]
+  content: [{ type: "p", 
+              body: "Having projects can help highlight certain experiences required for any job that you are applying." 
+            },
+            {
+              type: "p",
+              body: "You can add your project name along with the description of the project that you have done."
+            },
+            {
+             type: "p",
+             body: "With resuminator you can also add additional information, project link(github/gitlab etc.) and tags. "
+            }
+           ]
 };
 
 export default ProjectHints;
