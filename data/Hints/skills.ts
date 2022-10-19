019e7cdc481a010c421012deb73d5a1aa5aec64b
Sayandeep Ghosh <77008026+sayang14@users.noreply.github.com>
2022-10-19 15:42:54 +0530

    Update skills.ts

--- data/Hints/skills.ts
+++ data/Hints/skills.ts
@@ -22,7 +22,14 @@ import { SectionHints } from "./types";
 
 const SkillsHints: SectionHints = {
   title: "Adding skills to your resume",
-  content: [{ type: "p", body: "" }]
+  content: [{ type: "p", 
+              body: "Having skills shows the recruiters that you have the abilities required to succeed in the role in which you are applying." 
+            },
+            {
+              type: "p",
+              body: "With Resuminator you can add those skills in your resume more conveniently. You can mention the category under which your skill belongs. Foe example python belongs to programming language, keras belongs to machine learning framework tool."
+            }
+           ]
 };
 
 export default SkillsHints;
