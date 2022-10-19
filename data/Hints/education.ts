a615f577bb4d69e555916b19efdfe79bd3508844
Sayandeep Ghosh <77008026+sayang14@users.noreply.github.com>
2022-10-19 17:28:43 +0530

    Update education.ts

--- data/Hints/education.ts
+++ data/Hints/education.ts
@@ -23,19 +23,19 @@ import { SectionHints } from "./types";
 const EducationHints: SectionHints = {
   title: "Improving education info",
   content: [{ type: "p", 
-             body: "Education is one of the most important key sections that recruiters look for in the resume." 
+             body: "Education is an important key section that recruiters look for in the resume." 
             },
             {
              type: "p",
-             body: "You can add multiple degrees that you have obtained from your colleges as well as including your school degree." 
+             body: "You can add the degree that you have obtained from your colleges as well as including your school degree.You must add your most recent degree." 
             },
             {
              type: "p",
-             body: "With resuminator you can add your education in more details and arrange them in whichever way you like by clicking and holding the top right icon of each education section that you are adding!"
+             body: "With resuminator you can add your education in more details and arrange them in whichever way you like by clicking and holding the top right drag handle icon of each education section that you are adding!"
             },
             {
              type: "p",
-             body: "For filling your section in details just click the top right icon. After filling the details, you can collapse, hide or even delete them if you wish!"
+             body: "You can display both grades and percentage with resuminator. If you want convert your grade(CGPA,GPA,SGPA etc.) to percentage(%), just set the maximum value to 100 and it will be converted to percentage(%) in your resume"
             }
            ]
 };
